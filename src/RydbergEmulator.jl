d312cf2560d5e1ebb69206af20fa93e5ba506f7c
Roger-Luo <rogerluo.rl18@gmail.com>
2020-06-24 07:27:19 -0400

    support hamiltonian matrix constructor on cuda

--- src/RydbergEmulator.jl
+++ src/RydbergEmulator.jl
@@ -6,12 +6,19 @@ using SparseArrays
 using LightGraphs
 using LinearAlgebra
 using OrderedCollections
+using CUDA
+
+using LinearAlgebra: BlasReal, BlasComplex
+
 export RydInteract, RydAtom, XTerm, ZTerm, Hamiltonian
 export to_matrix, to_matrix!, update_term!
 
 include("atoms.jl")
 include("subspace.jl")
 include("hamiltonian.jl")
+
+include("register.jl")
+include("measure.jl")
 
 # using LightGraphs
 # using LinearAlgebra
@@ -31,9 +38,8 @@ include("hamiltonian.jl")
 # include("qaoa_mis.jl")
 
 
-# @static if CUDA.functional()
-#     using CUDA.CUSPARSE
-#     include("cuda.jl")
-# end
+@static if CUDA.functional()
+    include("cuda/cuda.jl")
+end
 
 end # module
--- src/cuda/hamiltonian.jl
+++ src/cuda/hamiltonian.jl
@@ -1,60 +1,21 @@
-function update_term!(dst::CuSparseMatrixCSR, n::Int, subspace_v::CuVector)
+function update_term!(dst::CuSparseMatrixCSR, t::AbstractTerm, subspace_v::Vector)
+    update_term!(dst, t, CuVector(subspace_v)) # copy to device, if subspace is given on CPU
 end
 
-function update_hamiltonian!(dst::CuSparseMatrixCSR, n::Int, subspace_v::CuVector, Ω, ϕ)
-    function kernel(rowPtr, colVal, nzVal, subspace_v, Ω, ϕ)
+function update_term!(dst::CuSparseMatrixCSR, t::AbstractTerm, subspace_v::CuVector)
+    function kernel(rowPtr, colVal, nzVal, subspace_v)
         row = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+
         if row > length(rowPtr) - 1
             return
         end
 
-        for k in rowPtr[row]:rowPtr[row+1]-1
+        @inbounds for k in rowPtr[row]:rowPtr[row+1]-1
             col = colVal[k]
             lhs = subspace_v[row]
             rhs = subspace_v[col]
-
-            mask = lhs ⊻ rhs
-            if lhs & mask == 0
-                nzVal[k] = Ω * exp(im * ϕ)
-            else
-                nzVal[k] = Ω * exp(-im * ϕ)
-            end
-            # update_x_term!(nzVal, lhs, rhs, k, Ω, ϕ)
+            update_nzval!(nzVal, k, t, col, row, rhs, lhs)
         end
-        return
-    end
-    
-    if size(dst, 2) < 256
-        threads = size(dst, 2)
-        nblocks = 1
-    else
-        threads = 256
-        nblocks = ceil(Int, size(dst, 2) / 256)
-    end
-
-    @cuda threads=threads blocks=nblocks kernel(dst.rowPtr, dst.colVal, dst.nzVal, subspace_v, Ω, ϕ)
-    return dst
-end
-
-function update_hamiltonian!(dst::CuSparseMatrixCSR, n::Int, subspace_v::CuVector, Ω, ϕ, Δ)
-    function kernel(rowPtr, colVal, nzVal, subspace_v, Ω, ϕ, Δ)
-        row = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-        if row > length(rowPtr) - 1
-            return
-        end
-
-        for k in rowPtr[row]:rowPtr[row+1]-1
-            col = colVal[k]
-            lhs = subspace_v[row]
-            rhs = subspace_v[col]
-
-            if row == col
-                update_z_term!(nzVal, lhs, n, k, Δ)
-            else
-                update_x_term!(nzVal, lhs, rhs, k, Ω, ϕ)
-            end
-        end
-        return
     end
 
     if size(dst, 2) < 256
@@ -65,6 +26,6 @@ function update_hamiltonian!(dst::CuSparseMatrixCSR, n::Int, subspace_v::CuVecto
         nblocks = ceil(Int, size(dst, 2) / 256)
     end
 
-    @cuda threads=threads blocks=nblocks kernel(dst.rowPtr, dst.colVal, dst.nzVal, subspace_v, Ω, ϕ, Δ)
+    @cuda threads=threads blocks=nblocks kernel(dst.rowPtr, dst.colVal, dst.nzVal, subspace_v)
     return dst
 end

7adb5f859977c97afb9edf51a0c757313c432cd7
Roger-luo <rogerluo.rl18@gmail.com>
2020-08-18 15:21:49 -0400

    add test

--- test/register.jl
+++ test/register.jl
@@ -7,4 +7,11 @@ using Yao
     raw_st = zeros(ComplexF64, length(subspace), 5)
     raw_st[1, :] .= 1
     @test state(RydbergEmulator.zero_state(10, Subspace(subspace); nbatch=5)) ≈ raw_st
+
+    raw_st = rand(ComplexF64, length(subspace), 5)
+    RydbergEmulator.RydbergReg(raw_st)
+
+    r = RydbergEmulator.RydbergReg{10}(raw_st, Subspace(subspace))
+    set_zero_state!(r)
+    @test all(r.state[1, :] .== 1)
 end

919226ee6b322d4baf8e22e3dadd4336426d1eec
Roger-luo <rogerluo.rl18@gmail.com>
2020-08-25 17:37:42 -0400

    move from RydbergEmulator

--- src/ContinuousEmulator.jl
+++ src/ContinuousEmulator.jl
@@ -2,6 +2,45 @@
 
 module ContinuousEmulator
 
-# Write your package code here.
+using Reexport
+@reexport using RydbergEmulator
+using OrdinaryDiffEq
+using Yao
+using SparseArrays
+using RydbergEmulator: AbstractTerm
+
+"""
+    shordinger(h::AbstractTerm, s::Subspace; cache=SparseMatrixCSC(h(1e-2), s))
+
+Create a Shordinger equation in given subspace `s`.
+"""
+function shordinger(h::AbstractTerm, s::Subspace; cache=SparseMatrixCSC(h(1e-2), s))
+    return function equation(dstate, state, h, t)
+        dstate .= -im .* update_term!(cache, h(t), s) * state
+    end
+end
+
+"""
+    shordinger(h::AbstractTerm; cache=SparseMatrixCSC(h(1e-2)))
+
+Create a Shordinger equation.
+"""
+function shordinger(h::AbstractTerm; cache=SparseMatrixCSC(h(1e-2)))
+    return function equation(dstate, state, h, t)
+        dstate .= -im .* update_term!(cache, h(t)) * state
+    end
+end
+
+function RydbergEmulator.emulate!(r::Yao.ArrayReg, t::Real, h::AbstractTerm; algo=Tsit5(), cache=SparseMatrixCSC(h(1e-2)), kwargs...)
+    prob = ODEProblem(shordinger(h; cache=cache), vec(r.state), (zero(t), t), h; save_everystep=false, save_start=false, alias_u0=true, kwargs...)
+    result = solve(prob, algo)
+    return r
+end
+
+function RydbergEmulator.emulate!(r::RydbergReg, t::Real, h::AbstractTerm; algo=Tsit5(), cache=SparseMatrixCSC(h(1e-2), r.subspace), kwargs...)
+    prob = ODEProblem(shordinger(h, r.subspace; cache=cache), vec(r.state), (zero(t), t), h; save_everystep=false, save_start=false, alias_u0=true, kwargs...)
+    result = solve(prob, algo)
+    return r
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,37 @@
+using LightGraphs
+using Yao
 using ContinuousEmulator
 using Test
 
-@testset "ContinuousEmulator.jl" begin
-    # Write your tests here.
+const test_graph = SimpleGraph(5)
+add_edge!(test_graph, 1, 2)
+add_edge!(test_graph, 2, 3)
+add_edge!(test_graph, 2, 4)
+add_edge!(test_graph, 2, 5)
+add_edge!(test_graph, 3, 4)
+add_edge!(test_graph, 4, 5)
+
+const test_subspace_v = [0, 1, 2, 4, 5, 8, 9, 16, 17, 20, 21]
+const test_subspace = Subspace(test_graph)
+
+@testset "contiguous time" begin
+    h = XTerm(5, 1.0, sin)
+    dt = 1e-5
+    @testset "subspace" begin
+        r1 = RydbergEmulator.zero_state(5, test_subspace)
+        emulate!(r1, 0.2, h)
+
+        r2 = RydbergEmulator.zero_state(5, test_subspace)
+        emulate!(r2, map(_->dt, 0.0:dt:0.2), map(h, 0.0:dt:0.2))
+
+        @test isapprox(r1.state, r2.state; atol=1e-4)
+    end
+
+    @testset "fullspace" begin
+        r1 = Yao.zero_state(5)
+        emulate!(r1, 0.2, h)
+        r2 = Yao.zero_state(5)
+        emulate!(r2, map(_->dt, 0.0:dt:0.2), map(h, 0.0:dt:0.2))
+        @test isapprox(r1.state, r2.state; atol=1e-4)
+    end
 end

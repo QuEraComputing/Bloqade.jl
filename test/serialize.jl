60f9c8d1d4335eed4d8fb55382d55188780e7ac4
Roger-Luo <rogerluo.rl18@gmail.com>
2021-02-15 16:19:06 -0500

    add to_dict test

--- test/serialize.jl
+++ test/serialize.jl
@@ -18,3 +18,19 @@ h2 = XTerm([0.1, 0.2, 0.3, 0.4, 0.5], [0.2, 0.3, 0.4, 0.5, 0.6]) +
 
 h3 = XTerm([0.1, 0.2, 0.3, 0.4, 0.5], 0.1) + ZTerm(5, 0.1) + RydInteract(atoms, 1.0)
 @test from_toml(Hamiltonian, "hamiltonians/h3.toml") == h3
+
+@test to_dict(h1) == OrderedDict{String, Any}(
+    "rydberg" => OrderedDict{String, Any}(
+        "atoms" => [[2, 2], [3, 5], [4, 4], [4, 5], [5, 4]],
+        "C" => 1.0,
+    ),
+    "xterm" => OrderedDict{String, Any}(
+        "nsites" => 5,
+        "omega" => 0.1,
+        "phi" => 0.2,
+    ),
+    "zterm" => OrderedDict{String, Any}(
+        "nsites" => 5,
+        "delta" => 0.1,
+    )
+)

eaf2d568d53d738099d7d7f3084a4c7119eb9c65
Rogerluo <rogerluo.rl18@gmail.com>
2021-05-13 19:46:54 -0400

    move MIS tools from MISExperimentUtils (#102)
    * change the default C
    
    * move mis tools from MISExperimentUtils

--- src/RydbergEmulator.jl
+++ src/RydbergEmulator.jl
@@ -11,6 +11,7 @@ using LinearAlgebra
 using OrderedCollections
 using Configurations
 using DelimitedFiles
+using EliminateGraphs
 using StatsBase
 
 import Yao
@@ -21,7 +22,7 @@ using LinearAlgebra: BlasReal, BlasComplex
 export RydInteract, RydAtom, XTerm, ZTerm, NTerm, Hamiltonian, EmulatorCache, RydbergReg, Subspace
 export to_matrix!, update_term!, simple_rydberg, rydberg_h, rydatoms, rand_atoms, read_atoms, write_atoms,
     unit_disk_graph, rand_unit_disk_graph, emulate!, emulate,
-    square_lattice, set_zero_state!, blockade_subspace,
+    square_lattice, set_zero_state!, blockade_subspace, is_independent_set, to_independent_set!,
     # reexport from Yao
     measure, zero_state,
     # units
@@ -41,7 +42,7 @@ include("emulate.jl")
 include("qaoa_mis.jl")
 
 include("serialize.jl")
-
+include("mis.jl")
 include("deprecations.jl")
 
 end # module
---
+++ src/mis.jl
@@ -0,0 +1,60 @@
+"""
+    is_independent_set(graph::AbstractGraph, config)
+
+Return `true` if `config` is an independent set of graph.
+`config` can be a `BitStr`, a vector, or any iterable.
+"""
+function is_independent_set(graph::AbstractGraph, config)
+    for i in 1:nv(graph)
+        for j in i+1:nv(graph)
+            config[i] == 1 && config[j] == 1 && has_edge(graph, i, j) && return false
+        end
+    end
+    return true
+end
+
+"""
+    to_independent_set!(graph::AbstractGraph, config::AbstractVector)
+
+Eliminate vertices in `config` so that remaining vertices do not have connected edges.
+This algorithm is a naive vertex elimination that does not nesesarily give the maximum possible vertex set.
+
+```@example
+# run the following code in Atom/VSCode
+atoms = RydAtom.([(0.0, 1.0), (1.0, 0.), (2.0, 0.0),
+    (1.0, 1.0), (1.0, 2.0), (2.0, 2.0)])
+graph = unit_disk_graph(atoms, 1.5)
+
+config = [1, 1, 1, 0, 1, 1]
+viz_config(atoms, graph, config)
+
+to_independent_set!(graph, config)
+viz_config(atoms, graph, config)
+```
+"""
+function to_independent_set!(graph::AbstractGraph, config::AbstractVector)
+    N = length(config)
+    n = typemax(Int)
+    while true
+        n, loc = findmax(map(i->num_mis_violation(config, graph, i), 1:N))
+        if n == 0
+            break
+        else
+            config[loc] = 0
+        end
+    end
+    return config
+end
+
+function num_mis_violation(config, graph::AbstractGraph, i::Int)
+    ci = config[i]
+    ci == 1 || return 0
+    count(j -> config[j] == 1 && has_edge(graph, i, j), 1:length(config))
+end
+
+"""
+    exact_solve_mis(g::AbstractGraph)
+
+Return the exact MIS size of a graph `g`.
+"""
+exact_solve_mis(g::AbstractGraph) = mis2(EliminateGraph(adjacency_matrix(g)))

28d2473fe5101c7edf5b36810880416eb08d8523
Roger-Luo <rogerluo.rl18@gmail.com>
2021-11-09 00:24:57 -0500

    fix doc

--- .ci/run.jl
+++ .ci/run.jl
@@ -15,6 +15,7 @@ function main()
 
     dev                 develop the packages in current environment
     test <device>       test the packages on cpu or cuda.
+    doc                 
     """
 
     length(ARGS) > 0 || return print(help)
@@ -33,6 +34,13 @@ function main()
             error("invalid device")
         end
         Pkg.test([package_names..., "EaRyd"]; coverage=true)
+    elseif "doc" == ARGS[1]
+        packages = map(package_names) do pkg
+            Pkg.PackageSpec(path = joinpath(root_directory, "lib", pkg))
+        end
+        push!(packages, Pkg.PackageSpec(path = root_directory))
+        Pkg.develop(packages)
+        Pkg.instantiate()
     end
 end
 

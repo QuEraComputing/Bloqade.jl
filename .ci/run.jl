9aa28a7022c030c4f00ac527ad2790ccaf31bedc
Roger-Luo <rogerluo.rl18@gmail.com>
2021-12-02 22:10:56 -0500

    improve CI test speed

--- .ci/run.jl
+++ .ci/run.jl
@@ -13,8 +13,8 @@ function main()
 
     Commands
 
-    dev                 develop the packages in current environment
-    test <device>       test the packages on cpu or cuda.
+    dev                         develop the packages in current environment
+    test <device> [--cuda]      test the packages on cpu or cuda.
     doc                 
     """
 
@@ -25,15 +25,19 @@ function main()
         end
         Pkg.develop(packages)
     elseif "test" == ARGS[1]
-        length(ARGS) == 2 || return print(help)
-        device = ARGS[2]
-        if device == "cuda"
-        elseif device == "cpu"
+        length(ARGS) ≤ 3 || return print(help)
+
+        if length(ARGS) == 2 && "--cpu" in ARGS # test all on cpu
             package_names = filter(!startswith("Cu"), package_names)
+        elseif length(ARGS) == 2 && "--cuda" in ARGS # test all on cuda
+            push!(package_names, "EaRyd")
+        elseif length(ARGS) == 2
+            ARGS[2] == "EaRyd" || ARGS[2] in package_names || return print(help)
+            package_names = [ARGS[2]]
         else
-            error("invalid device")
+            return print(help)
         end
-        Pkg.test([package_names..., "EaRyd"]; coverage=true)
+        Pkg.test(package_names; coverage=true)
     elseif "doc" == ARGS[1]
         packages = map(package_names) do pkg
             Pkg.PackageSpec(path = joinpath(root_directory, "lib", pkg))

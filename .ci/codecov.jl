e2afd6cb71302271d0a7573186b38fcbc86a2a9d
Roger-Luo <rogerluo.rl18@gmail.com>
2021-12-03 11:28:33 -0500

    fix code coverage

--- .ci/codecov.jl
+++ .ci/codecov.jl
@@ -21,7 +21,11 @@ function main()
         package_names = ARGS
     end
     dirs = map(package_names) do name
-        joinpath(root_directory, "lib", name, "src")
+        return if name == "EaRyd"
+            joinpath(root_directory, "src")
+        else
+            joinpath(root_directory, "lib", name, "src")
+        end
     end
     pfs = mapreduce(process_folder, vcat, dirs)
     LCOV.writefile(joinpath(root_directory, "lcov.info"), pfs)

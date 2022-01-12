c753ff50ae99afcbcde83ea71d6282dfa7559c45
Roger-Luo <rogerluo.rl18@gmail.com>
2022-01-12 14:07:35 -0500

    polish bitstring plot

--- lib/EaRydPlots/test/runtests.jl
+++ lib/EaRydPlots/test/runtests.jl
@@ -1,11 +1,10 @@
-using Makie
+using Test
 using CairoMakie
-using MakieCore
 using EaRydCore
+using EaRydPlots
 
-@macroexpand @recipe(BitStringHistgram) do scene
+@testset "test plot" begin
+    r = rand_state(12)
+    plt = bitstring_histgram(r)
+    @test size(plt.layout) == (1, 1)
 end
-
-
-r = rand_state(12)
-plt = bitstring_histgram(r)

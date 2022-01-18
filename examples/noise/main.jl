0a2714492bf5b464532620248276a0be520b9a55
fanglifl <lflnankai@gmail.com>
2022-01-18 14:28:37 -0500

    add text for the example

--- examples/noise/main.jl
+++ examples/noise/main.jl
@@ -2,16 +2,19 @@ using EaRyd
 using CairoMakie
 using EaRydPlots
 
-# In this example, we examine the adiabatic preparation of a spin Hamiltonian.  
+# In this example, we examine the adiabatic preparation of a spin Hamiltonian with noise parameters to show the erro propagation feature.  
 
 
 # We first prepare the adiabatic pulse sequence as two piecewise linear functions
+# the first part considers Hamiltonian parameters with time-dependent global noise (where each atom site is subject to the same noise)
+
 # define the rabi waveform
 Ω_src = 2.3 * 2 * pi
+# the noise strength can be specified by \pm
 Ω_max = Ω_src ± 0.001
 Ω = piecewise_linear(clocks=[0.0, 0.252, 1.052, 1.6], values=[0.0, Ω_max , Ω_max , 0])
 
-# define the detuning waveform
+# define the noisy detuning waveform
 U = Ω_src / 2.3 ± 0.001
 Δ = piecewise_linear(clocks=[0.0, 0.252, 1.052, 1.6], values=[-6*U, -6*U, 2*U , 2*U])
 
@@ -23,7 +26,7 @@ nsites = nx*ny
 atoms = generate_sites(SquareLattice(), nx, ny, scale=9.629)
 
 
-# We construct the Rydberg Hamiltonian from the defined rabi and detuning waveforms
+# We construct the Rydberg Hamiltonian from the defined noisy rabi and detuning waveforms
 h = rydberg_h(atoms; C=2 * pi * 858386, Δ, Ω)
 
 using SparseArrays
@@ -31,13 +34,17 @@ using SparseArrays
 prob = ODEEvolution(zero_state(9), 1.6±0.0, h)
 emulate!(prob) # run the time evolution directly
 
-# We compute the Rydberg probability and plot the histogram of the most frequent results from sampling the final state 
+# We compute the Rydberg probability for each site, where each of the results contains an error bar
 densities = map(1:nsites) do i
     real(expect(put(nsites, i=>Op.n), prob.reg))
 end
 
 
 
+# the second part considers Hamiltonian parameters with time-dependent local noise (where each atom site is subject to independent noise)
+
+
+# each atom is subject to an indenpendent noise
 Ω = map(1:9) do idx
     piecewise_linear(clocks=[0.0, 0.252, 1.052, 1.6], values=[0.0, Ω_max, Ω_max , 0])
 end
@@ -46,9 +53,14 @@ end
     piecewise_linear(clocks=[0.0, 0.252, 1.052, 1.6], values=[-6*U, -6*U, 2*U , 2*U])
 end
 
+
+# define the Hamiltonian 
+
 h = rydberg_h(atoms; C=2 * pi * 858386, Δ, Ω)
 prob = ODEEvolution(zero_state(9), 1.6±0.0, h)
 emulate!(prob) # run the time evolution directly
+
+# measure the Rydberg density for each site
 densities = map(1:nsites) do i
     real(expect(put(nsites, i=>Op.n), prob.reg))
 end

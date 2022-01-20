b42e5839b51f444e82dea414b9c91c849e583b4a
GiggleLiu <cacate0129@gmail.com>
2022-01-20 09:34:56 -0500

    update plot

--- lib/EaRydLattices/src/visualize.jl
+++ lib/EaRydLattices/src/visualize.jl
@@ -6,7 +6,9 @@ struct Rescaler{T}
     pad::T
 end
 
-getscale(r::Rescaler) = 1/(r.xmax-r.xmin+r.pad)
+#getscale(r::Rescaler) = 1/(r.xmax-r.xmin+r.pad)
+getscale(r::Rescaler) = min(1/(r.xmax-r.xmin+r.pad), 1/(r.ymax-r.ymin+r.pad))
+getnodescale(r::Rescaler) = min(1/(r.xmax-r.xmin+r.pad), 1/(r.ymax-r.ymin+r.pad))
 
 function (r::Rescaler{T})(x; dims=(1,2)) where T
     xmin, ymin, xmax, ymax, pad = r.xmin, r.ymin, r.xmax, r.ymax, r.pad
@@ -30,15 +32,15 @@ function get_rescaler(atoms::AbstractVector{<:Tuple}, pad)
     return Rescaler(xmin, xmax, ymin, ymax, pad)
 end
 
-default_node_style(scale; color="white") = compose(context(), Viznet.nodestyle(:default, r=0.15cm*scale), Compose.stroke("black"), fill(color), linewidth(0.3mm*scale))
-default_text_style(scale) = Viznet.textstyle(:default, fontsize(4pt*scale))
-default_bond_style(scale; color="black") = Viznet.bondstyle(:default, Compose.stroke(color), linewidth(0.3mm*scale))
+default_node_style(scale, stroke_color, fill_color) = compose(context(), Viznet.nodestyle(:default, r=0.15cm*scale), Compose.stroke(stroke_color), fill(fill_color), linewidth(0.3mm*scale))
+default_text_style(scale, color) = Viznet.textstyle(:default, fontsize(4pt*scale), fill(color))
+default_bond_style(scale, color) = Viznet.bondstyle(:default, Compose.stroke(color), linewidth(0.3mm*scale))
 default_line_style_grid(scale) = Viznet.bondstyle(:default, Compose.stroke("#AAAAAA"), linewidth(0.3mm*scale); dashed=true)
-default_blockade_style(scale, blockade_radius) = Viznet.nodestyle(:circle, Compose.fill("transparent"), r=blockade_radius, Compose.stroke("black"), strokedash([0.5mm*scale, 0.5mm*scale]), linewidth(0.3mm*scale))
+default_blockade_style(scale, blockade_radius, stroke_color) = Viznet.nodestyle(:circle, Compose.fill("transparent"), r=blockade_radius, Compose.stroke(stroke_color), strokedash([0.5mm*scale, 0.5mm*scale]), linewidth(0.3mm*scale))
 
 """
-    viz_atoms([io::Union{IO,AbstractString}, ]atoms::AtomList;
-        colors=fill("white", count(maskedgrid.mask)), blockade_radius=0,
+    img_atoms(atoms::AtomList;
+        colors=["black", "black", ...], blockade_radius=0,
         texts=["1", "2", ...],
         blockade_radius=0;
         format=PNG,
@@ -58,29 +60,20 @@ julia> open("test.png", "w") do f
 
 The `format` keyword argument can also be `Compose.SVG` or `Compose.PDF`.
 """
-function viz_atoms(io, atoms::AtomList; format=PNG,
-        colors=fill("white", length(atoms)),
+function img_atoms(atoms::AtomList;
+        colors=nothing,
         blockade_radius=0,
-        texts = ["$i" for i=1:length(atoms)],
+        texts = nothing,
+        format=SVG, io=nothing,
         kwargs...)
-    img, (dx, dy) = img_atoms(atoms; colors=colors, blockade_radius=blockade_radius, texts=texts, config=LatticeDisplayConfig(; kwargs...))
-    Compose.draw(format(io, dx, dy), img)
-    return
-end
-function viz_atoms(atoms::AtomList;
-        colors=fill("white", length(atoms)),
-        blockade_radius=0,
-        texts = ["$i" for i=1:length(atoms)],
-        kwargs...)
-    img, (dx, dy) = img_atoms(atoms; colors=colors, blockade_radius=blockade_radius, texts=texts, config=LatticeDisplayConfig(; kwargs...))
-    Compose.set_default_graphic_size(dx, dy)
-    return img
+    img, (dx, dy) = viz_atoms(atoms; colors=colors, blockade_radius=blockade_radius, texts=texts, config=LatticeDisplayConfig(; kwargs...))
+    return (io === nothing ? format(dx, dy) : format(io, dx, dy))(img)
 end
 
 function _edges(atoms, blockade_radius)
     n = length(atoms)
     edges = Tuple{Int,Int}[]
-    for i=1:n, j=1:n
+    for i=1:n, j=i+1:n
         if sum(abs2, atoms[i] .- atoms[j]) <= blockade_radius ^ 2
             push!(edges, (i, j))
         end
@@ -88,39 +81,65 @@ function _edges(atoms, blockade_radius)
     return edges
 end
 
+function fit_image(rescaler::Rescaler, image_size, imgs...)
+    X = rescaler.xmax - rescaler.xmin + rescaler.pad
+    Y = rescaler.ymax - rescaler.ymin + rescaler.pad
+    img_rescale = image_size/max(X, Y)*cm
+    if Y < X
+        return Compose.compose(context(0, 0, 1.0, X/Y), imgs...), (X*img_rescale, Y*img_rescale)
+    else
+        return Compose.compose(context(0, 0, Y/X, 1.0), imgs...), (X*img_rescale, Y*img_rescale)
+    end
+end
+
 # Returns a 2-tuple of (image::Context, size)
-function img_atoms(al::AtomList; colors, blockade_radius, texts, config)
-    @assert length(al) == length(colors)
-    @assert length(al) == length(texts)
+function viz_atoms(al::AtomList; colors, blockade_radius, texts, config)
     atoms = padydim(al).atoms
-    rescaler = get_rescaler(atoms, 2*config.scale)
-    X = rescaler.xmax - rescaler.xmin + 2*rescaler.pad
-    Y = rescaler.ymax - rescaler.ymin + 2*rescaler.pad
-    img = _viz_atoms(rescaler.(atoms), _edges(atoms, blockade_radius), colors, texts, config, getscale(rescaler)*blockade_radius)
-    img_rescale = 12cm/max(X, Y)
-    return Compose.compose(context(0, 0, 1.0, X/Y), img), (X*img_rescale, Y*img_rescale)
+    rescaler = get_rescaler(atoms, 2.0)
+    img = _viz_atoms(rescaler.(atoms), _edges(atoms, blockade_radius), colors, texts, config, blockade_radius, getnodescale(rescaler))
+    return fit_image(rescaler, config.image_size, img)
 end
 
 Base.@kwdef struct LatticeDisplayConfig
-    scale::Real = 1.5
+    # line, node and text
+    scale::Float64 = 1.0
+    text_color::String = "black"
+
+    node_stroke_color = "black"
+    node_fill_color = "white"
     # bond
     bond_color::String = "black"
     # blockade
     blockade_style::String = "none"
+    blockade_stroke_color::String = "black"
+    # image size in cm
+    image_size::Float64 = 18
 end
 
-function _viz_atoms(locs, edges, colors, texts, config, blockade_radius)
-    node_styles = [default_node_style(config.scale; color=color) for color in colors]
-    edge_style = default_bond_style(config.scale; color=config.bond_color)
-    blockade_radius_style = default_blockade_style(config.scale, (config.blockade_style=="half" ? blockade_radius/2 : blockade_radius))
-    text_style = default_text_style(config.scale)
+function _viz_atoms(locs, edges, colors, texts, config, blockade_radius, rescale)
+    radi = (config.blockade_style=="half" ? blockade_radius/2 : blockade_radius)*rescale
+    rescale = rescale * config.image_size * config.scale * 1.6
+    if colors !== nothing
+        @assert length(locs) == length(colors)
+        node_styles = [default_node_style(rescale, config.node_stroke_color, color) for color in colors]
+    else
+        node_styles = fill(default_node_style(rescale, config.node_stroke_color, config.node_fill_color), length(locs))
+    end
+    if texts !== nothing
+        @assert length(locs) == length(texts)
+    end
+    edge_style = default_bond_style(rescale, config.bond_color)
+    blockade_radius_style = default_blockade_style(rescale, radi, config.blockade_stroke_color)
+    text_style = default_text_style(rescale, config.text_color)
     Viznet.canvas() do
         for (i, node) in enumerate(locs)
             node_styles[i] >> node
             if config.blockade_style != "none"
                 blockade_radius_style >> node
             end
-            text_style >> (node, texts[i])
+            if config.text_color !== "transparent"
+                text_style >> (node, texts === nothing ? "$i" : texts[i])
+            end
         end
         for (i, j) in edges
             edge_style >> (locs[i], locs[j])
@@ -129,7 +148,7 @@ function _viz_atoms(locs, edges, colors, texts, config, blockade_radius)
 end
 
 """
-    viz_maskedgrid([io::Union{IO,AbstractString}, ]maskedgrid::MaskedGrid;
+    img_maskedgrid([io::Union{IO,AbstractString}, ]maskedgrid::MaskedGrid;
         format=PNG,
         blockade_radius = blockade_radius,
         colors=fill("white", count(maskedgrid.mask)))
@@ -137,30 +156,26 @@ end
 Draw a `maskedgrid` with colors specified by `colors` and texts specified by `texts`.
 You will need a `VSCode`, `Pluto` notebook or `Jupyter` notebook to show the image.
 """
-function viz_maskedgrid(io, maskedgrid::MaskedGrid;
-        format=PNG,
-        colors=fill("white", count(maskedgrid.mask)),
-        texts = ["$i" for i=1:sum(maskedgrid.mask)],
+function img_maskedgrid(maskedgrid::MaskedGrid;
+        format=SVG, io=nothing,
+        colors=nothing,
+        texts = nothing,
         blockade_radius = 0,
         kwargs...
         )
-    img, (dx, dy) = img_maskedgrid(maskedgrid; colors=colors, texts=texts, blockade_radius=blockade_radius, config=LatticeDisplayConfig(; kwargs...))
-    Compose.draw(format(io, dx, dy), img)
-    return
+    img, (dx, dy) = viz_maskedgrid(maskedgrid; colors=colors, texts=texts, blockade_radius=blockade_radius, config=LatticeDisplayConfig(; kwargs...))
+    return (io === nothing ? format(dx, dy) : format(io, dx, dy))(img)
 end
 
 # Returns a 2-tuple of (image::Context, size)
-function img_maskedgrid(maskedgrid::MaskedGrid; colors, texts, config, blockade_radius)
+function viz_maskedgrid(maskedgrid::MaskedGrid; colors, texts, config, blockade_radius)
     atoms = collect_atoms(maskedgrid)
-    rescaler = get_rescaler(atoms, 2*config.scale)
-    X = rescaler.xmax - rescaler.xmin + rescaler.pad
-    Y = rescaler.ymax - rescaler.ymin + rescaler.pad
+    rescaler = get_rescaler(atoms, 2.0)
     line_style_grid = default_line_style_grid(config.scale)
-    img1 = _viz_atoms(rescaler.(atoms), _edges(atoms, blockade_radius), colors, texts, config, getscale(rescaler)*blockade_radius)
-    img2 = _viz_grid(rescaler.(maskedgrid.xs; dims=1), rescaler.(maskedgrid.ys; dims=2), line_style_grid, Y/X)
-    img = compose(context(0, 0, 1.0, X/Y), (context(), img1), (context(), img2))
-    img_rescale = 12cm/max(X, Y)
-    return img, (X*img_rescale, Y*img_rescale)
+    img1 = _viz_atoms(rescaler.(atoms), _edges(atoms, blockade_radius), colors, texts, config, blockade_radius, getnodescale(rescaler))
+    ymax = (rescaler.ymax - rescaler.ymin + 2*rescaler.pad)/(rescaler.xmax - rescaler.xmin + 2*rescaler.pad)
+    img2 = _viz_grid(rescaler.(maskedgrid.xs; dims=1), rescaler.(maskedgrid.ys; dims=2), line_style_grid, ymax)
+    fit_image(rescaler, config.image_size, img1, img2)
 end
 function _viz_grid(xs, ys, line_style, ymax)
     Viznet.canvas() do
@@ -180,11 +195,11 @@ for (mime, format) in [MIME"image/png"=>PNG, MIME"text/html"=>SVG]
         end
     
         function Base.show(io::IO, ::$mime, maskedgrid::MaskedGrid)
-            viz_maskedgrid(io, maskedgrid; format=$format)
+            img_maskedgrid(maskedgrid; format=$format, io=io)
         end
     
         function Base.show(io::IO, ::$mime, list::AtomList)
-            viz_atoms(io, list; format=$format)
+            img_atoms(list; format=$format, io=io)
         end
     end
 end

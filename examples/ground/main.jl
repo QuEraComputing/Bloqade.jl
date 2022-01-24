554063ea4457c78f004a4a119d8ae8232976e902
fanglifl <lflnankai@gmail.com>
2022-01-24 12:18:07 -0500

    calculate ladder

--- examples/ground/main.jl
+++ examples/ground/main.jl
@@ -1,13 +1,12 @@
 
 # write your EaRyd example with Literate.jl here
-
-
 # calculate the ground states of a ladder system
+
 using EaRyd
 using KrylovKit
 using SparseArrays
 
-n_sites= 10
+n_sites= 8
 L = (n_sites - 1)* ax
 atomx= collect(0.0:ax:L)
 
@@ -25,18 +24,33 @@ for ii= 1: n_sites
     push!(atom_coordinate, (atomx[ii], ay2))
 end
 
-h = rydberg_h(atom_coordinate; C=1.5^6, Δ=3, Ω=1)
+h = rydberg_h(atom_coordinate; C=2.4^6, Δ=3.5, Ω=1)
 
 H0 = SparseMatrixCSC(h)
-vals, vecs, info = KrylovKit.eigsolve(H0,  1, :SR)
+vals, vecs, info = KrylovKit.eigsolve(H0,  4, :SR)
 state1= ArrayReg(vecs[1]) 
+state2= ArrayReg(vecs[2])
+
+state3= ArrayReg((vecs[1]-vecs[2])/sqrt(2))
+
 
 output_mat1= zeros(Float64, 2*n_sites)
+output_mat2= zeros(Float64, 2*n_sites)
+output_mat3= zeros(Float64, 2*n_sites)
+
 
 
 for i in 1:2*n_sites
-    observe = real(expect(put(2*n_sites, i=>Op.n), state1))
-    output_mat1[i] = observe
+    observe1 = real(expect(put(2*n_sites, i=>Op.n), state1))
+    observe2 = real(expect(put(2*n_sites, i=>Op.n), state2))
+    observe3 = real(expect(put(2*n_sites, i=>Op.n), state3))
+
+    output_mat1[i] = observe1
+    output_mat2[i] = observe2
+    output_mat3[i] = observe3
+
 end
 
-print(output_mat1)
+println(output_mat1)
+println(output_mat2)
+println(output_mat3)

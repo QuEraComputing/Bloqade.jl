bf43e2af0c0247677441ebf00fb11a8edf33ecf0
Roger-Luo <rogerluo.rl18@gmail.com>
2022-01-24 02:44:00 -0500

    support subspace

--- lib/EaRydPlots/src/bitstring_hist.jl
+++ lib/EaRydPlots/src/bitstring_hist.jl
@@ -24,9 +24,26 @@ function bitstring_histgram!(plt, r::ArrayReg; nlargest::Int=10, title="")
     return plt
 end
 
+function bitstring_histgram!(plt, r::RydbergReg; nlargest::Int=10, title="")
+    state = statevec(r)
+    probs = abs2.(state)
+    indices = find_largest(probs, nlargest)
+
+    ax = Axis(
+        plt;
+        xticks = (1:length(indices), string.(r.subspace.subspace_v[indices];base=2, pad=nqubits(r))),
+        xticklabelrotation=π/3,
+        title,
+        xlabel="bitstring",
+        ylabel="probability"
+    )
+    barplot!(ax, 1:length(indices), probs[indices])
+    return plt
+end
+
 """
 """
-function bitstring_histgram(r::ArrayReg; nlargest::Int=10, title="")
+function bitstring_histgram(r; nlargest::Int=10, title="")
     fig = Figure()
     bitstring_histgram!(fig[1, 1], r; nlargest, title)
     return fig

38789072699a390b0843cb9694b9b756e9b8d597
Roger-Luo <rogerluo.rl18@gmail.com>
2022-01-24 01:42:32 -0500

    add basic waveform plotting

--- lib/EaRydPlots/test/runtests.jl
+++ lib/EaRydPlots/test/runtests.jl
@@ -9,5 +9,11 @@ using EaRydPlots
     @test size(plt.layout) == (1, 1)
 end
 
-r = rand_state(12)
-plt = bitstring_histgram(r)
+# r = rand_state(12)
+# plt = bitstring_histgram(r)
+
+
+# using EaRydWaveforms
+# wf = piecewise_linear(clocks=[0.0, 1.0, 2.0, 3.0], values=[0.0, 2.0, 2.0, 1.0])
+
+# EaRydPlots.draw(wf)

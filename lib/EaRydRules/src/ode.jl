2746a7fd2326aabda25faa96c0507abecfea576b
GiggleLiu <cacate0129@gmail.com>
2022-02-26 17:55:45 -0500

    update ode

--- lib/EaRydRules/src/ode.jl
+++ lib/EaRydRules/src/ode.jl
@@ -21,6 +21,29 @@ prob = ODEProblem(field_function!, u0, tspan, [2.0])
 integrator = init(prob, Vern8(); dt=0.01)
 step!(integrator, 0.01)
 
+# t
+# dt
+# uprev
+# destats.nf  # number of fevals
+
+# fixed
+# ---------------------
+# alg    # algorithm
+# opts   # options
+# f
+# 
+# not useful
+# ---------------------
+# u  # overwritten
+# p  # parameters
+# EEst   # error estimation, not used.
+# eigen_est # ?, not used
+
+# k      # f values.
+
+# cache.tab.extra
+# k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,utilde,tmp,rtmp,atmp
+
 # option ref: https://diffeq.sciml.ai/stable/basics/common_solver_opts/#solver_options
 function diffode(; alg=Vern8(),
         dt=0.01, alg_hint=:nonstiff)

91490eb479f3af858775f64e064e97ebbd67772d
Roger-luo <rogerluo.rl18@gmail.com>
2022-03-29 22:01:00 +0000

    fix test

--- lib/EaRydExpr/test/interface.jl
+++ lib/EaRydExpr/test/interface.jl
@@ -4,8 +4,8 @@ using YaoBlocks.Optimise
 
 @testset "rydberg_h" begin
     positions = [(1, 2), (2, 3)]
-    @test rydberg_h(positions; Ω=1.0) ==
-        RydInteract(;atoms=positions) + SumOfX(;nsites=2, Ω=0.5)
+    h = RydInteract(;atoms=positions) + SumOfX(;nsites=2, Ω=0.5)
+    @test rydberg_h(positions; Ω=1.0) == Optimise.simplify(h)
 
     h = RydInteract(;atoms=positions) + SumOfX(;nsites=2, Ω=0.5) -
         SumOfN(;nsites=2, Δ=0.2)

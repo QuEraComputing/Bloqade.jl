4ff0c389d61deb2d66c6317d4822bb48a5b2d966
Roger-luo <rogerluo.rl18@gmail.com>
2022-04-15 03:20:34 +0000

    fix tests

--- lib/BloqadeKrylov/test/emulate.jl
+++ lib/BloqadeKrylov/test/emulate.jl
@@ -9,7 +9,7 @@ using Yao
 
 @testset "emulate" begin
     atoms = generate_sites(ChainLattice(), 4, scale=3.2);
-    clocks=[0.0, 0.5, 0.8, 1.1, 1.5]
+    clocks=[0.0, 0.5, 0.8, 1.1, 1.5, 1.6]
     wf = piecewise_constant(clocks=clocks, values=[0.0, 2.1, 2.1, 1.5, 0.0])
     h = rydberg_h(atoms; Ω=wf)
     reg = zero_state(length(atoms))

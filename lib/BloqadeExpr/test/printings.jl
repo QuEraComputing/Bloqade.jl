1a4f5b15c8a5061b9d9f04919202b5c28af6eeb7
Roger-luo <rogerluo.rl18@gmail.com>
2022-04-15 04:58:44 +0000

    print phase

--- lib/BloqadeExpr/test/printings.jl
+++ lib/BloqadeExpr/test/printings.jl
@@ -18,6 +18,21 @@ for h in [
     rydberg_h(atoms; Ω=[1.0, 2.0], Δ=[sin, sin]),
     rydberg_h(atoms; Ω=sin, Δ=[1.0, 2.0]),
     rydberg_h(atoms; Ω=[sin, sin], Δ=[1.0, 2.0]),
+    
+    rydberg_h(atoms; Ω=1.0, ϕ=sin),
+    rydberg_h(atoms; Ω=1.0, ϕ=[sin, sin]),
+    rydberg_h(atoms; Ω=sin, ϕ=1.0),
+    rydberg_h(atoms; Ω=[sin, sin], ϕ=1.0),
+
+    rydberg_h(atoms; Ω=2.0, ϕ=sin),
+    rydberg_h(atoms; Ω=2.0, ϕ=[sin, sin]),
+    rydberg_h(atoms; Ω=sin, ϕ=2.0),
+    rydberg_h(atoms; Ω=[sin, sin], ϕ=2.0),
+
+    rydberg_h(atoms; Ω=[1.0, 2.0], ϕ=sin),
+    rydberg_h(atoms; Ω=[1.0, 2.0], ϕ=[sin, sin]),
+    rydberg_h(atoms; Ω=sin, ϕ=[1.0, 2.0]),
+    rydberg_h(atoms; Ω=[sin, sin], ϕ=[1.0, 2.0]),
 ]
 
     show(stdout, MIME"text/plain"(), h)

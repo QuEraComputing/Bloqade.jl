b810f28ab6f264f46fa2f2ae4681e978e439df38
Roger-luo <rogerluo.rl18@gmail.com>
2022-04-15 18:50:01 +0000

    add basic deps & add execute

--- lib/BloqadeSchema/src/BloqadeSchema.jl
+++ lib/BloqadeSchema/src/BloqadeSchema.jl
@@ -1,5 +1,7 @@
 module BloqadeSchema
 
+using BloqadeExpr
+using BloqadeWaveforms
 using Configurations
 
 export TaskSpecification
@@ -7,5 +9,6 @@ export TaskSpecification
 include("types.jl")
 include("serialize.jl")
 include("deserialize.jl")
+include("execute.jl")
 
 end
---
+++ lib/BloqadeSchema/src/execute.jl
@@ -0,0 +1,5 @@
+function execute(task::TaskSpecification)
+end
+
+function generate_json(h)
+end

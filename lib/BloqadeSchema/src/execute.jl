ba2f6850426d41cff2c6855bcfa932760561997a
Roger-luo <rogerluo.rl18@gmail.com>
2022-04-15 18:57:05 +0000

    setup interface def & add notes

--- lib/BloqadeSchema/src/execute.jl
+++ lib/BloqadeSchema/src/execute.jl
@@ -1,5 +1,13 @@
-function execute(task::TaskSpecification)
+function to_hamiltonian(task::TaskSpecification)
 end
 
-function generate_json(h)
+function to_json(h::AbstractBlock)
+    # 1. check if the input block expression is a summation
+    # of RydInteract, SumOfX, SumOfXPhase, SumOfN
+    # 2. extract the atom positions, Ω, ϕ, Δ
+    # 3. generate corresponding pulses in QuEraSchema
+    # 4. call to_dict on QuEraSchema then using JSON3
+    #    to convert the dict to json string
+    # NOTE: let's ignore local pulse pattern for now
+    # since in our representation it's all local pulse
 end

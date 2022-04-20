c41f81f45afde8519492aee19a4562d6b50c73b7
Rogerluo <rogerluo.rl18@gmail.com>
2022-04-19 22:15:31 -0400

    polish printing (#265)
    * polish printing
    
    * add precompile

--- lib/BloqadeExpr/src/printings.jl
+++ lib/BloqadeExpr/src/printings.jl
@@ -35,23 +35,33 @@ function latex_expr(t::YaoBlocks.Scale)
 end
 
 function print_expr(io::IO, ::MIME"text/plain", t::RydInteract)
-    C = round(t.C, sigdigits=3)
-    print(io, "∑ $C/|r_i-r_j|^6 n_i n_j")
+    C = t.C/2π
+    n = ceil(log10(C))
+    C = round(C / 10^n, digits=3)
+    print(io, "∑ 2π ⋅ $(C)e$n/|r_i-r_j|^6 n_i n_j")
 end
 
 function latex_expr(t::RydInteract)
-    C = round(t.C, sigdigits=3)
-    return "\\sum \\frac{$C}{|r_i-r_j|^6} n_i n_j"
+    C = t.C/2π
+    n = ceil(log10(C))
+    C = round(C / 10^n, digits=3)
+    return "\\sum \\frac{2π \\cdot $C\\times 10^{$n}}{|r_i-r_j|^6} n_i n_j"
+end
+
+function pretty_number(x)
+    return if isone(x)
+        ""
+    elseif isone(x/2π)
+        "2π ⋅ "
+    else
+        x = round(x/2π, sigdigits=3)
+        "2π ⋅ $x ⋅ "
+    end
 end
 
 function print_expr(io::IO, ::MIME"text/plain", t::SumOfX)
     if t.Ω isa Number
-        if isone(t.Ω)
-            print(io, "∑ σ^x_i")
-        else
-            Ω = round(t.Ω, sigdigits=3)
-            print(io, "$Ω ⋅ ∑ σ^x_i")
-        end
+        print(io, pretty_number(t.Ω), "∑ σ^x_i")
     elseif t.Ω isa Vector
         print(io, "∑ Ω_i ⋅ σ^x")
     else
@@ -61,12 +71,7 @@ end
 
 function latex_expr(t::SumOfX)
     if t.Ω isa Number
-        if isone(t.Ω)
-            tex = "\\sum σ^x_i"
-        else
-            Ω = round(t.Ω, sigdigits=3)
-            tex = "$Ω ⋅ \\sum σ^x_i"
-        end
+        tex = pretty_number(t.Ω) * "\\sum σ^x_i"
     elseif t.Ω isa Vector
         tex = "\\sum Ω_i ⋅ σ^x"
     else
@@ -78,12 +83,7 @@ end
 function print_expr(io::IO, ::MIME"text/plain", t::Union{SumOfN, SumOfZ})
     op = t isa SumOfN ? "n_i" : "σ^z_i"
     if t.Δ isa Number
-        if isone(t.Δ)
-            print(io, "∑ $op")
-        else
-            Δ = round(t.Δ, sigdigits=3)
-            print(io, "$Δ ⋅ ∑ $op")
-        end
+        print(io, pretty_number(t.Δ), "∑ $op")
     elseif t.Δ isa Vector
         print(io, "∑ Δ_i ⋅ $op")
     else
@@ -94,12 +94,7 @@ end
 function latex_expr(t::Union{SumOfN, SumOfZ})
     op = t isa SumOfN ? "n_i" : "σ^z_i"
     if t.Δ isa Number
-        if isone(t.Δ)
-            tex = "\\sum $op"
-        else
-            Δ = round(t.Δ, sigdigits=3)
-            tex = "$Δ ⋅ \\sum $op"
-        end
+        tex = pretty_number(t.Δ) * "\\sum $op"
     elseif t.Δ isa Vector
         tex = "\\sum Δ_i ⋅ $op"
     else
@@ -111,42 +106,42 @@ end
 function print_expr(io::IO, ::MIME"text/plain", t::SumOfXPhase)
     @switch (t.Ω, t.ϕ) begin
         @case (Ω::Vector, ϕ::Vector)
-            Ω = is_time_function(Ω) ? "Ω(t)_i" : "Ω_i"
-            ϕ = is_time_function(ϕ) ? "ϕ(t)_i" : "ϕ_i"
+            Ω = is_const_param(Ω) ? "Ω_i" : "Ω(t)_i"
+            ϕ = is_const_param(ϕ) ? "ϕ_i" : "ϕ(t)_i"
             print(io, "∑ $Ω ⋅ (e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|)")
         @case (Ω::Vector, ϕ)
-            Ω = is_time_function(Ω) ? "Ω(t)_i" : "Ω_i"
-            ϕ = is_time_function(ϕ) ? "ϕ(t)" : round(ϕ, sigdigits=3)
+            Ω = is_const_param(Ω) ? "Ω_i" : "Ω(t)_i"
+            ϕ = is_const_param(ϕ) ? round(ϕ, sigdigits=3) : "ϕ(t)"
             print(io, "∑ $Ω ⋅ (e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|)")
         @case (Ω, ϕ::Vector)
-            Ω = is_time_function(Ω) ? "Ω(t)" : round(Ω, sigdigits=3)
-            ϕ = is_time_function(ϕ) ? "ϕ(t)_i" : "ϕ_i"
-            print(io, "$Ω ⋅ ∑ e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|")
+            Ω = is_const_param(Ω) ? pretty_number(Ω) : "Ω(t) ⋅"
+            ϕ = is_const_param(ϕ) ? "ϕ_i" : "ϕ(t)_i"
+            print(io, Ω, "∑ e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|")
         @case (Ω, ϕ)
-            Ω = is_time_function(Ω) ? "Ω(t)" : round(Ω, sigdigits=3)
-            ϕ = is_time_function(ϕ) ? "ϕ(t)" : round(ϕ, sigdigits=3)
-            print(io, "$Ω ⋅ ∑ e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|")
+            Ω = is_const_param(Ω) ? pretty_number(Ω) : "Ω(t) ⋅"
+            ϕ = is_const_param(ϕ) ? round(ϕ, sigdigits=3) : "ϕ(t)"
+            print(io, Ω, "∑ e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|")
     end
 end
 
 function latex_expr(t::SumOfXPhase)
     @switch (t.Ω, t.ϕ) begin
         @case (Ω::Vector, ϕ::Vector)
-            Ω = is_time_function(Ω) ? "Ω(t)_i" : "Ω_i"
-            ϕ = is_time_function(ϕ) ? "ϕ(t)_i" : "ϕ_i"
+            Ω = is_const_param(Ω) ? "Ω_i" : "Ω(t)_i"
+            ϕ = is_const_param(ϕ) ? "ϕ_i" : "ϕ(t)_i"
             tex = "\\sum $Ω ⋅ (e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|)"
         @case (Ω::Vector, ϕ)
-            Ω = is_time_function(Ω) ? "Ω(t)_i" : "Ω_i"
-            ϕ = is_time_function(ϕ) ? "ϕ(t)" : round(ϕ, sigdigits=3)
+            Ω = is_const_param(Ω) ? "Ω_i" : "Ω(t)_i"
+            ϕ = is_const_param(ϕ) ? round(ϕ, sigdigits=3) : "ϕ(t)"
             tex = "\\sum $Ω ⋅ (e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|)"
         @case (Ω, ϕ::Vector)
-            Ω = is_time_function(Ω) ? "Ω(t)" : round(Ω, sigdigits=3)
-            ϕ = is_time_function(ϕ) ? "ϕ(t)_i" : "ϕ_i"
-            tex = "$Ω ⋅ \\sum e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|"
+            Ω = is_const_param(Ω) ? pretty_number(Ω) : "Ω(t) ⋅ "
+            ϕ = is_const_param(ϕ) ? "ϕ_i" : "ϕ(t)_i"
+            tex = Ω * "\\sum e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|"
         @case (Ω, ϕ)
-            Ω = is_time_function(Ω) ? "Ω(t)" : round(Ω, sigdigits=3)
-            ϕ = is_time_function(ϕ) ? "ϕ(t)" : round(ϕ, sigdigits=3)
-            tex = "$Ω ⋅ \\sum e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|"
+            Ω = is_const_param(Ω) ? pretty_number(Ω) : "Ω(t) ⋅ "
+            ϕ = is_const_param(ϕ) ? round(ϕ, sigdigits=3) : "ϕ(t)"
+            tex = Ω * "\\sum e^{$ϕ ⋅ im} |0⟩⟨1| + e^{-$ϕ ⋅ im} |1⟩⟨0|"
     end
     return tex
 end

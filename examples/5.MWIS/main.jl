27697e61d116dc3453f85dd3aed46c684c632976
Minh-Thi Nguyen <84551256+minhthin1028@users.noreply.github.com>
2022-05-03 17:39:13 -0400

    Update main.jl

--- examples/5.MWIS/main.jl
+++ examples/5.MWIS/main.jl
@@ -1,6 +1,6 @@
 # # Maximum Weight Independent Set
 # ## Background
-# In the [The Maximum Independent Set Problem](@id mis-tutorial) page, 
+# In the [The Maximum Independent Set Problem](@ref mis-tutorial) page, 
 # we provide a detailed example on how to solve the Maximum Independent Set (MIS) problem
 # using neutral-atom quantum computers. 
 # The first algorithm proposal and experimental demonstration can be found in 

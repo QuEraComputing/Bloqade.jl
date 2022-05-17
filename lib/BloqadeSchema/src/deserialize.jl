bbc1b0a092ae48357a4b1a9f7a4b99984e1d87d8
Marc Bacvanski <mbacvanski@quera.com>
2022-05-17 13:45:48 -0400

    cleanup deserialize

--- lib/BloqadeSchema/src/deserialize.jl
+++ lib/BloqadeSchema/src/deserialize.jl
@@ -4,17 +4,14 @@ function Configurations.from_dict(::Type{Lattice}, ::Type{NTuple{2,Float64}}, x)
     return (x[1], x[2])
 end
 
-# function Configurations.from_dict(::Type{RydbergRabiFrequencyAmplitude}, d::Dict{String,OrderedDict{String,Any}})
 function Configurations.from_dict(::Type{RydbergRabiFrequencyAmplitude}, d::Dict{String,<:Any})
     return RydbergRabiFrequencyAmplitude(from_dict(RydbergRabiFrequencyAmplitudeGlobal, d["global"]))
 end
 
-# function Configurations.from_dict(::Type{RydbergRabiFrequencyPhase}, d::Dict{String,OrderedDict{String,Any}})
 function Configurations.from_dict(::Type{RydbergRabiFrequencyPhase}, d::Dict{String,<:Any})
     return RydbergRabiFrequencyPhase(from_dict(RydbergRabiFrequencyPhaseGlobal, d["global"]))
 end
 
-# function Configurations.from_dict(::Type{RydbergDetuning}, d::Dict{String,OrderedDict{String,Any}})
 function Configurations.from_dict(::Type{RydbergDetuning}, d::Dict{String,<:Any})
     return if haskey(d, "local")
         RydbergDetuning(

d3b87b9d3d9356806d38e8664f6c96b7d18c0453
Sofia Gonzalez <gonzalez.gar.sofia@gmail.com>
2022-07-18 08:45:36 +0200

    improve units.jl test

--- lib/BloqadeExpr/test/units.jl
+++ lib/BloqadeExpr/test/units.jl
@@ -20,7 +20,6 @@ unitless_values = [5, 0:0.1:1, [1,2,3]]
 unit_values = [5m, 0cm:0.1cm:1cm, [1mm,2mm,3mm]]
 conversion_exponent = [6, 4, 3]
 
-#test for a value::Quantity, range::AbstractRange and vector::AbstractArray{S} where S<:Quantity
-@testset "basic unit conversion tests" for i in 1:3
-    @test default_unit(μm, unit_values[i]) == unitless_values[i]*10^conversion_exponent[i]
+@testset "convert $(typeof(unitful))" for (unitless, unitful, exponent) in zip(unitless_values, unit_values, conversion_exponent)
+    @test default_unit(μm, unitful) == unitless*10^exponent
 end

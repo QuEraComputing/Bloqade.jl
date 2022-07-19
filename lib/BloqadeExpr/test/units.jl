12761884c6c900ae2a47265b3cdbb1660144ff66
Anna Knörr <61286790+Atomyka@users.noreply.github.com>
2022-07-19 11:37:22 +0200

    addressed pr change requests

--- lib/BloqadeExpr/test/units.jl
+++ lib/BloqadeExpr/test/units.jl
@@ -1,12 +1,16 @@
 using Test
 using BloqadeExpr
-using Unitful: kHz, µHz, Hz, µm, m
+using BloqadeExpr: default_unit
+using Unitful: kHz, µHz, Hz, MHz, µm, m, cm, mm
 
 @testset "term units" begin
     @test SumOfXPhase(5, 2kHz, 1.0).Ω ≈ 0.002
     @test SumOfX(5, 2kHz).Ω ≈ 0.002
     @test SumOfZ(5, 2kHz).Δ ≈ 0.002
     @test SumOfN(5, 2kHz).Δ ≈ 0.002
+    @test SumOfXPhase(5, 2kHz, 2.0).ϕ ≈ 2.0
+
+    @test SumOfX(3, [2μHz,2Hz,2MHz]).Ω ≈ [2*1e-12,2*1e-6,2]
 
     h = RydInteract(atoms = [(1,), (2,), (3,), (4)], C = 109.2kHz * µm^6)
     @test h.C ≈ 0.1092
@@ -14,8 +18,10 @@ using Unitful: kHz, µHz, Hz, µm, m
     @test h.C ≈ 0.1092
 end
 
-@testset "further units" begin
-    @test SumOfXPhase(5, 2kHz, 2.0).ϕ ≈ 2.0 # Should test line 4, i.e. NoUnits version of default_unit
-    @test default_unit(μm, (1.0:0.2:2.0)m) ≈ (1.0:0.2:2.0)* 1e6 # Should test lines 11-16, i.e. Range version of default unit. Note that Range does not seem to be used commonly in Hamiltonian terms (e.g. SumOfX, except for in Adiabatic Evolution)
-    @test SumOfX(3, [2μHz,2Hz,2MHz]).Ω ≈ [2*1e-12,2*1e-6,2] # Should test lines 18-24, i.e. the Array version of default_unit
+unitless_values = [5, 0:0.1:1, [1,2,3]]
+unit_values = [5m, 0cm:0.1cm:1cm, [1mm,2mm,3mm]]
+conversion_exponent = [6, 4, 3]
+
+@testset "convert $(typeof(unitful))" for (unitless, unitful, exponent) in zip(unitless_values, unit_values, conversion_exponent)
+    @test default_unit(μm, unitful) == unitless*10^exponent
 end

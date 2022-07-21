1bf1a74292bae459e59521a36002d22e76768159
maolinml <86260930+maolinml@users.noreply.github.com>
2022-07-21 10:28:07 -0700

    [WIP] add get_average_rydberg_densities to src/observables.jl (#379)
    * add get_average_rydberg_densities to src/observables.jl
    
    * update
    
    * update
    
    * Update src/observables.jl
    
    Co-authored-by: Rogerluo <hiroger@qq.com>
    
    * update per comment
    
    * further update
    
    * update
    
    * undo the previous update
    
    * minus change and a simple test added
    
    * it should be minor change and a simple test added
    
    * update to fix the export
    
    Co-authored-by: Mao Lin <maonlinml@amazon.com>
    Co-authored-by: Rogerluo <hiroger@qq.com>

--- test/observables.jl
+++ test/observables.jl
@@ -7,3 +7,11 @@ reg = rand_state(10)
 
 @test rydberg_corr(reg) ≈
       [expect(chain(nqubits(reg), put(i => Op.n), put(j => Op.n)), reg) for i in 1:nqubits(reg), j in 1:nqubits(reg)]
+
+
+# Test 1 for get_average_rydberg_densities: π-pulse for 1-atom
+@test [[0.0], [1.0]] ≈ get_average_rydberg_densities(
+      generate_sites(ChainLattice(), 1), 
+      zero_state(1); 
+      Ω = piecewise_linear(clocks = [0.0, 0.1], values = [π/(0.1), π/(0.1)]), 
+      dt=0.1)

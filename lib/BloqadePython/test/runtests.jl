d3fffd4078f202f718cbea43e572a5bd05fb3a64
Roger-luo <rogerluo.rl18@gmail.com>
2022-07-29 13:55:53 -0400

    fix python wrapper, let's just do all the conversions in python

--- lib/BloqadePython/test/runtests.jl
+++
@@ -1,6 +0,0 @@
-using BloqadePython
-using Test
-
-@testset "BloqadePython.jl" begin
-    # Write your tests here.
-end
--- python/bloqade/__init__.py
+++ python/bloqade/__init__.py
@@ -2,12 +2,19 @@ __version__ = '0.1.0'
 import juliacall
 import numpy as np
 juliacall.Main.seval('using Bloqade')
-# juliacall.Main.seval('using BloqadePython')
+juliacall.Main.seval('using PythonCall')
+
 bloqade = juliacall.Main.seval('Bloqade')
+pyconvert = juliacall.Main.seval('pyconvert')
 
 X = bloqade.X
 Y = bloqade.Y
 Z = bloqade.Z
+
+VectorOfInt = juliacall.Main.seval(r'Vector{Int}')
+VectorOfFloat64 = juliacall.Main.seval(r'Vector{Float64}')
+VectorOfVectorOfFloat64 = juliacall.Main.seval(r'Vector{Vector{Float64}}')
+atom_pos_type = VectorOfVectorOfFloat64
 
 def rydberg_h(atom_positions, C = 2 * np.pi * 862690, omega=None, phi = None, delta = None):
     return bloqade.rydberg_h(atom_positions, C, omega, phi, delta)
@@ -15,17 +22,18 @@ def rydberg_h(atom_positions, C = 2 * np.pi * 862690, omega=None, phi = None, de
 def _convert_pylist_atom_positions(atoms):
     positions = np.array(atoms)
     ndims = len(positions.shape)
-    positions = positions.reshape((positions.shape[0], 1))
-    return [each for each in positions]
+    if ndims == 1: # list of 1D positions
+        positions = positions.reshape((positions.shape[0], 1))
+
+    return pyconvert(atom_pos_type, [[coor for coor in pos] for pos in positions])
 
 def unit_disk_graph(atoms, radius = 1):
     atoms = _convert_pylist_atom_positions(atoms)
     return bloqade.unit_disk_graph(atoms, radius)
 
 def mis_postprocessing(config, graph, ntrials : int =10):
-    return bloqade.mis_postprocessing(config, graph, ntrials=ntrials)
-
-
+    jl_configs = bloqade.mis_postprocessing(pyconvert(VectorOfInt, config), graph, ntrials=ntrials)
+    return np.array(jl_configs)
 
 class AbstractBlock(juliacall.AnyValue):
 

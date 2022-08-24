765c41514ebf629b478d2c17f6604bba1a28d51a
Phillip Weinberg <weinbe58@gmail.com>
2022-08-24 09:10:05 -0400

    adding doc string to function

--- lib/BloqadeWaveforms/src/descretize.jl
+++ lib/BloqadeWaveforms/src/descretize.jl
@@ -86,6 +86,21 @@ function descretize(wf::Waveform{PiecewiseConstant{T},T};
 
 end
 
+"""
+    descretize(waveform;[max_vale=Inf64,max_slope=Inf64,min_step=0.0])
+
+Function which takes a waveform and translates it to a linear interpolation subject to some constraints. The function returns a piecewise linear waveform. if the Waveform is piecewise linear only the constraints will be checked. 
+
+# Arguments
+
+- `waveform`: ['Waveform'](@ref)  to be descretized.
+
+# Keyword Arguments
+
+- `max_value`: Maximium absoluate value waveform can take
+- `max_slope`: Maximum possible slope used in interpolation
+- `tol`: tolerance of interpolation, this is a bound to the area between the linear interpolation and the waveform.
+"""
 function descretize(wf::Waveform; 
     max_value::Real=Inf64, 
     max_slope::Real=Inf64, 

9e869e097dab9407171232a359ec8433e6952579
Phillip Weinberg <weinbe58@gmail.com>
2022-08-29 12:03:54 -0400

    fixing broken test

--- lib/BloqadeSchema/test/execute.jl
+++ lib/BloqadeSchema/test/execute.jl
@@ -8,6 +8,7 @@ using JSON
 
 @testset "to_schema" begin
     T = 2.0
+    s = 0.2
     atoms = [(i,0) for i in 1:10]
     Δ = Waveform(t->cos(2π*t/T),T)
     # Δi=Δ
@@ -15,14 +16,12 @@ using JSON
     Δi = [a*Δ for a in Amps]
 
     Ω = Waveform(t->sin(2π*t/T)^2,T)
-    ϕ = piecewise_constant(;clocks=[0,T/2,T],values=[0,π])
-    # ϕ = Waveform(t->π*sin(π*t/2/T)^2,T)
+    ϕ = piecewise_linear(;clocks=[0,T/2-s,T/2+s,T],values=[0.0,0.0,π,π])
 
     H = rydberg_h(atoms;Ω=Ω,Δ=Δi,ϕ=ϕ)
 
-    h = BloqadeSchema.to_json(H,waveform_tolerance=1e-1)
+    h = BloqadeSchema.to_json(H,waveform_tolerance=1e-3)
     H2 = BloqadeSchema.from_json(h)
-
-    println(H2)
+    
 end
 

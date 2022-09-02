0d2e36d733f9687da5c10d2e05ebd872aefff524
fanglifl <lflnankai@gmail.com>
2022-09-02 14:25:12 -0400

    add tutorial example schema

---
+++ generate_schema/1D_Z2.jl
@@ -0,0 +1,26 @@
+using BloqadeSchema
+using Bloqade
+using JSON
+
+
+# preparing 1D Z2 phase. For more details, see https://queracomputing.github.io/Bloqade.jl/dev/tutorials/2.adiabatic/main/
+
+total_time = 3.0;
+Ω_max = 2π * 4;
+Ω = piecewise_linear(clocks = [0.0, 0.1, 2.1, 2.2, total_time], values = [0.0, Ω_max, Ω_max, 0, 0]);
+
+U1 = -2π * 10;
+U2 = 2π * 10;
+Δ = piecewise_linear(clocks = [0.0, 0.6, 2.1, total_time], values = [U1, U1, U2, U2]);
+
+nsites = 9
+atoms = generate_sites(ChainLattice(), nsites, scale = 5.72)
+
+
+H = rydberg_h(atoms; Δ, Ω)
+h = BloqadeSchema.to_json(H,waveform_tolerance=1e-1)
+
+
+open("generate_schema/1D_Z2.json","w") do f
+    JSON.print(f, h)
+end

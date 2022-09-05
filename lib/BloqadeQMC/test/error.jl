95c4e9dbb323933d4789b94fdb82cb94d98a353f
Anna Knoerr <an.kn@protonmail.ch>
2022-09-05 09:58:39 +0200

    added jackknife unit tests

---
+++ lib/BloqadeQMC/test/error.jl
@@ -0,0 +1,23 @@
+using Test
+using Statistics: mean, std
+using Measurements: value, uncertainty
+# using BloqadeQMC: jackknife
+using Jackknife: estimate, variance, bias
+
+include("../src/error.jl")
+
+@testset "simple numerical tests for jackknife" begin
+    x = [3,2,11,7,6]
+    
+    # Resampling should not change estimate when f = mean. 
+    m = mean(x)
+    @test value(jackknife(mean, x)) ≈ m  
+
+    # Resampling should give a better estimate for e.g. f = inv. We check this using the Julia library Jackknife
+    g(x) = 1/mean(x) # The estimate function from Jackknife takes needs g to be explicity defined as a function of the mean
+    @test value(jackknife(inv, x)) ≈ estimate(g, x)
+    @test uncertainty(jackknife(inv, x)) ≈ sqrt(variance(g, x))
+
+    # Test jackknife when dispatched on f = identity
+    @test std(x) > uncertainty(jackknife(x))
+end

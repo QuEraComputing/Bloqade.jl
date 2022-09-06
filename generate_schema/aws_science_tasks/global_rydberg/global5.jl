7114e500306969d26578c88aa7817730a02afb91
fanglifl <lflnankai@gmail.com>
2022-09-06 19:02:46 -0400

    update task

--- generate_schema/aws_science_tasks/global_rydberg/global5.jl
+++ generate_schema/aws_science_tasks/global_rydberg/global5.jl
@@ -34,7 +34,7 @@ T_pi_2 = 1/16
 T= 0.1
 epsilon =0.05
 Ω = piecewise_constant(clocks=[0.0, epsilon, T_pi_2+ epsilon, T + T_pi_2+epsilon, T + 2* T_pi_2+epsilon, T + 2* T_pi_2+2* epsilon], values= 2π*[0.0, 4, 0, 4, 0])
-ϕ = piecewise_constant(clocks=[0.0, epsilon, T_pi_2+ epsilon, T + T_pi_2+epsilon, T + 2* T_pi_2+epsilon, T + 2* T_pi_2+2* epsilon], values= 2π*[0.0, 0, 0, 1/4, 0])
+ϕ = piecewise_constant(clocks=[0.0, epsilon, T_pi_2+ epsilon, T + T_pi_2+epsilon, T + 2* T_pi_2+epsilon, T + 2* T_pi_2+2* epsilon], values= 2π*[0.0, 0, 0, 0, 0])
 
 
 H = rydberg_h(atoms; Ω=Ω,  ϕ = ϕ)
@@ -45,4 +45,19 @@ open("generate_schema/aws_science_tasks/global_rydberg/global5.json","w") do f
     JSON.print(f, h)
 end
 
+# task 3
 
+atoms = AtomList([(0.0, 0.0)])
+
+T_pi_2 = 1/16
+T= 0.1
+epsilon =0.005
+Ω = piecewise_constant(clocks=[0.0, epsilon, T_pi_2+ epsilon, T/2 + T_pi_2+epsilon, T/2 + 3* T_pi_2+epsilon, T + 3* T_pi_2+epsilon, T + 4* T_pi_2+epsilon, T + 4* T_pi_2+2* epsilon], values= 2π*[0.0, 4, 0, 4, 0, 4, 0])
+
+H = rydberg_h(atoms;Ω=Ω)
+h = BloqadeSchema.to_json(H,waveform_tolerance=1e-1,warn=true)
+
+
+open("generate_schema/aws_science_tasks/global_rydberg/global5.json","w") do f
+    JSON.print(f, h)
+end

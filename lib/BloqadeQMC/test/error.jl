7de44c7532b39218a496c1eaaaa220c68bc231cb
Anna Knoerr <an.kn@protonmail.ch>
2022-09-19 16:43:43 +0200

    more jackknife tests

--- lib/BloqadeQMC/test/error.jl
+++ lib/BloqadeQMC/test/error.jl
@@ -2,7 +2,10 @@ using Test, BloqadeQMC
 using Statistics: mean, std
 using Measurements: value, uncertainty
 using BloqadeQMC: jackknife
-using Jackknife: estimate, variance, bias
+using Random
+using RandomNumbers
+
+rng = Xorshifts.Xoroshiro128Plus(1234)
 
 @testset "simple numerical tests for jackknife" begin
     x = [3,2,11,7,6]
@@ -12,10 +15,24 @@ using Jackknife: estimate, variance, bias
     @test value(jackknife(mean, x)) ≈ m  
 
     # Resampling should give a better estimate for e.g. f = inv. We check this using the Julia library Jackknife
-    g(x) = 1/mean(x) # The estimate function from Jackknife takes needs g to be explicity defined as a function of the mean
-    @test value(jackknife(inv, x)) ≈ estimate(g, x)
-    @test uncertainty(jackknife(inv, x)) ≈ sqrt(variance(g, x))
+    @test value(jackknife(inv, x)) ≈ 0.158110765
+    @test uncertainty(jackknife(inv, x)) ≈ 0.052468798
+end
 
-    # Test jackknife when dispatched on f = identity
-    @test std(x) > uncertainty(jackknife(x))
+@testset "functional tests for jackknife" begin
+
+    mean_val =  5.0
+    x =  fill(mean_val, 100) + randn!(rng, zeros(100))
+    j_error = uncertainty(jackknife(x))
+    #check jackknifed std < original sample std
+    @test j_error < std(x)  
+
+    add(x,y,z) = x+y+z
+    num_measurements_list = [10,100,1000,10000,100000]
+    #create a set of normally random values about a mean value for x,y,z for different measuremensts
+    xs_list = [[fill(mean_val, num_measurements_list[n]) + randn!(rng, zeros(num_measurements_list[n])) for v in 1:3] for n in 1:length(num_measurements_list)]
+    jackknife_errors = [uncertainty(jackknife(add, xs_list[n][1], xs_list[n][2], xs_list[n][3])) for n in 1:length(num_measurements_list)]
+    #test that error decreases as the number of observations gets larger
+    @test issorted(jackknife_errors, lt=Base.isgreater)
+
 end

8db44f3dff769b44b7dfd6464a5c17e48656b283
Phillip Weinberg <weinbe58@gmail.com>
2022-09-20 08:36:20 -0400

    moving functions from BloqadeSchema to BloqadeExpr

--- lib/BloqadeSchema/src/parse.jl
+++ lib/BloqadeSchema/src/parse.jl
@@ -1,50 +1,13 @@
 
 
 
-function mult_by_two(Ω)
-    isnothing(Ω) && return
-    if BloqadeExpr.is_const_param(Ω)
-        return Ω .* 2
-    end
-
-    return if Ω isa Vector
-        map(Ω) do Ω_i
-            return Ω_i.f
-        end
-    else
-        Ω.f
-    end
-
-end
-
-
-function get_rydberg_params(h::BloqadeExpr.RydbergHamiltonian)
-    # extracts parameters from RydbergHamiltonian
-    ϕ = nothing
-    Ω = nothing
-    Δ = nothing
-
-    if h.rabi_term isa BloqadeExpr.SumOfX
-        Ω = mult_by_two(h.rabi_term.Ω)
-    elseif h.rabi_term isa BloqadeExpr.SumOfXPhase
-        Ω = mult_by_two(h.rabi_term.Ω)
-        ϕ = h.rabi_term.ϕ
-    end
-
-    if h.detuning_term isa BloqadeExpr.SumOfN
-        Δ = h.detuning_term.Δ
-    end
-
-    return (h.rydberg_term.atoms,ϕ,Ω,Δ)
-end
-
 
 schema_parse_field(::Symbol,field::PiecewiseLinearWaveform) = field
 schema_parse_field(name::Symbol,::Any) = error("Cannot convert Hamiltonian to schema, $name must be piecewise linear Waveform.")
 
 
 
-"""
+#= 
 schema_parse_ϕ(ϕ::Waveform{BloqadeWaveforms.PiecewiseLinear{T,I},T}) where {T<:Real,I} = ϕ
 schema_parse_Ω(Ω::Waveform{BloqadeWaveforms.PiecewiseLinear{T,I},T}) where {T<:Real,I} = Ω
 
@@ -94,12 +57,13 @@ function schema_parse_Δ(Δ)
         error("Cannot convert Hamiltonian to schema, Δ(t) must be a single or collection of piecewise linear Waveform(s).")
     end
 end
-"""
+=#
+
 # pass fail function. 
 # certain criteria is required to pass
 # 1. all waveforms must be PWL
 # 2. Local detuning must be PWL with all same clocks and can be decomposed 
-#    with SVD such that the waveform is Δ(i,t) = Δ(t) + Δ_i * δ(t)
+#    the waveform is Δ(i,t) = Δ(t) + Δ_i * δ(t)
 function schema_parse_rydberg_fields(H::BloqadeExpr.RydbergHamiltonian)
     atoms,ϕ,Ω,Δ = get_rydberg_params(H)
     ϕ = schema_parse_field(:ϕ,ϕ)

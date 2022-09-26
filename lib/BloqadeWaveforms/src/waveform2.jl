d9cf09ceda2da4ca8fa397ee5c1d1c040f187df5
Roger-luo <rogerluo.rl18@gmail.com>
2022-09-26 14:10:20 -0400

    save draft

--- lib/BloqadeWaveforms/src/waveform2.jl
+++ lib/BloqadeWaveforms/src/waveform2.jl
@@ -41,7 +41,7 @@ end
 
 struct Smooth <: AbstractWaveform
     # must be piecewise(linear/constant)
-    waveform::Vector{Piecewise}
+    waveform::Piecewise
     kernel::Symbol
     edge_pad_size::Int
 end
@@ -49,8 +49,45 @@ end
 # TODO: check this with Harry, if this should be named as "pulse"
 # or it should be named as "channel", or something better
 
-# attach the waveform 
-struct Pulse # or WaveformList?
-    waveforms::Vector{Any} # atom label => AbstractWaveform
-    glob # global channel
+struct Channel
+    label # => atom
+    # individual
+    rabi_waveform
+    detuning_waveform
+    # address::UInt64 # rabi/detuning 0x10000123
+    # waveforms # waveforms in the same channel will be execute in parallel
 end
+
+struct RabiPattern
+    mask::Vector{UInt8}
+    waveform
+end
+
+struct DetuningPattern
+    amp::Vector{Float64}
+    waveform
+end
+
+struct RydbergProgram
+    rabi::Vector{RabiPattern}
+    detuning::Vector{DetuningPattern}
+end
+
+prog = RydbergProgram()
+
+quote
+    Δ(t) = 2t
+    Ω(t) = 3t
+
+    rydberg_h(Δ, Ω)
+end
+
+Vector{DetuningPattern}
+
+0x00010000
+0x01110110
+0x00010010
+
+Δ(i,t) = Δ_i*δ(i)
+
+wf = δ/10 *(0+0+....1) = δ/10

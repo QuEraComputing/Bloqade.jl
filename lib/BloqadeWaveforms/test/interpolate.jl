55c112992cda1b4fbb5e43ba47ec7eb79ce31284
Phillip Weinberg <weinbe58@gmail.com>
2022-09-28 13:40:58 -0400

    Weinbe58/piecewise constant interp (#427)
    * first implementation of piecewise_constant_interpolation
    
    * Export piecewise_constant_interpolate
    
    * piecewise_constant_interpolate works but suffers performance issues, tolerance needs to be potentially adjusted/investigated for existing unit tests
    
    * Added piecewise_constant_interpolate definition for already piecewise constant waveforms and associated unit tests
    
    * adding functionality for piecewise constant interpolation along with some basic tests
    
    * Added function to check if point exists in/on parallelogram in semi-open interval
    
    * removed in_parallelogram, move to separate branch
    
    Co-authored-by: John Long <johnzl@protonmail.ch>

--- lib/BloqadeWaveforms/test/interpolate.jl
+++ lib/BloqadeWaveforms/test/interpolate.jl
@@ -7,6 +7,50 @@ step_msg = "Requested tolerance for interpolation violates the step size constra
 constraint_msg = "Interpolation requires either a tolerance constraint or a slope and step constraint."
 warn_msg = "negative tolerance provided, taking absolute value."
 
+function benchmark_func() 
+    wf = Waveform(t->t^2,1)
+    new_wf = piecewise_constant_interpolate(wf; atol=0,min_step=1e-3)
+end
+
+@testset "piecewise constant" begin
+
+    @testset "constraint terminated" begin
+        wf = Waveform(t->t^2,1)
+        new_wf = piecewise_constant_interpolate(wf; atol=0,min_step=1e-3)
+        @test norm(wf - new_wf) < 1e-1
+    end
+
+    @testset "general waveform" begin
+
+        f_list = [(t->t^2,4.0),(t->sin(t),2π)]
+        atol=1e-2
+        for (function_number, (f,duration)) in enumerate(f_list)
+            wf = Waveform(f,duration)
+            new_wf = piecewise_constant_interpolate(wf;atol=atol)
+    
+            @test isapprox(wf,new_wf,atol=atol)
+        end
+    
+        wf = Waveform(t->t^2,2)
+    
+        @test_logs (:warn,warn_msg) piecewise_linear_interpolate(wf;atol=-1e-5)
+        @test_throws ErrorException piecewise_linear_interpolate(wf;min_step = 0.1)
+        @test_throws ErrorException piecewise_linear_interpolate(wf;atol=0)
+    
+    end
+
+    @testset "piecewise constant waveforms" begin
+        wf = piecewise_constant(;clocks = [0.0, 2.0, 3.0, 4.0], values = [0.0, 2.0, 1.0])
+        new_wf = piecewise_constant_interpolate(wf)
+        @test wf == new_wf
+
+        step_msg = "Waveform step smaller than constraint."
+        @test_logs (:warn,warn_msg) piecewise_constant_interpolate(wf;atol=-1e-5)
+        @test_throws ErrorException piecewise_constant_interpolate(wf;atol=0)
+        @test_throws ErrorException piecewise_constant_interpolate(wf;min_step=3.0)
+    end
+
+end
 
 @testset "piecewise linear waveforms" begin
     wf = piecewise_linear(clocks = [0.0, 2.0, 3.0, 4.0], values = [0.0, 3.0, 1.1, 2.2])
@@ -62,8 +106,6 @@ end
     @test_throws ErrorException piecewise_linear_interpolate(wf;max_slope = 2.0)
     @test_throws ErrorException piecewise_linear_interpolate(wf;min_step = 0.1)
     @test_throws ErrorException piecewise_linear_interpolate(wf;atol=0)
-
-
 
 end
 

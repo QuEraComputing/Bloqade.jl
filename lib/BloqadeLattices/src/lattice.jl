552ef955b983a35a9d0e476cbfa6ab54b899e269
Phillip Weinberg <weinbe58@gmail.com>
2022-10-04 13:56:29 -0400

    Update lib/BloqadeLattices/src/lattice.jl

--- lib/BloqadeLattices/src/lattice.jl
+++ lib/BloqadeLattices/src/lattice.jl
@@ -30,7 +30,7 @@ end
     GeneralLattice(vectors, sites)
 
 The general lattice type for tiling the space. Type parameter `D` is the dimension,
-`K` is the number of sites in a unit region and `T` is the data type for coordinates, e.g. `Float64`. Input arguments are
+`K` is the number of sites in a unit cell and `T` is the data type for coordinates, e.g. `Float64`. Input arguments are
 
 * `vectors` is a vector/tuple of D-tuple. Its length is D, it specifies the Bravais lattice vectors.
 * `sites` is a vector/tuple of D-tuple. Its length is K, it specifies the sites inside a Bravais cell.

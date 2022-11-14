b955c5bffbfff9afde03ba51eeaee872357939b4
Phillip Weinberg <weinbe58@gmail.com>
2022-11-14 11:32:58 -0500

    changing example used in test

--- lib/BloqadeKrylov/test/forwarddiff.jl
+++ lib/BloqadeKrylov/test/forwarddiff.jl
@@ -6,19 +6,33 @@ using BloqadeWaveforms
 using BloqadeKrylov
 using BloqadeLattices
 
-ForwardDiff.derivative(2.0) do x
+function test(x;tol=1e-7)
+    T = 0.1
+    dT = T/10
     reg = zero_state(Complex{typeof(x)}, 5)
-    atoms = [(i,) for i in 1:5]
-    h = rydberg_h(atoms; Ω = sin, Δ = x)
-    prob = KrylovEvolution(reg, 0:1e-3:0.1, h)
+    atoms = [(6.7*i,) for i in 1:5]
+    h = rydberg_h(atoms; Ω = x, Δ = t->-5*cos(π*t/T))
+    prob = KrylovEvolution(reg, 0:dT:T, h;tol=tol)
     emulate!(prob)
-    return abs2(statevec(reg)[1])
+    return abs2.(statevec(reg))
 end
 
-using ExponentialUtilities
+test(10.0;tol=1e-9)
 
-ForwardDiff.derivative(2.0) do x
-    st = rand(2)
-    st = BloqadeKrylov.expmv(2.0, fill(x, (2, 2)), st)
-    return sum(st)
-end
+
+# ForwardDiff.derivative(2.0) do x
+#     reg = zero_state(Complex{typeof(x)}, 5)
+#     atoms = [(i,) for i in 1:5]
+#     h = rydberg_h(atoms; Ω = sin, Δ = x)
+#     prob = KrylovEvolution(reg, 0:1e-3:0.1, h)
+#     emulate!(prob)
+#     return abs2(statevec(reg)[1])
+# end
+
+# using ExponentialUtilities
+
+# ForwardDiff.derivative(2.0) do x
+#     st = rand(2)
+#     st = BloqadeKrylov.expmv(2.0, fill(x, (2, 2)), st)
+#     return sum(st)
+# end

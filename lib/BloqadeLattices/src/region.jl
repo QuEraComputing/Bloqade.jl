aed1cc72e8c67d46b9706149361a44c08979fcba
John Long <johnzl@protonmail.ch>
2022-11-14 14:50:14 -0500

    added docstrings for distnace in region.jl

--- lib/BloqadeLattices/src/region.jl
+++ lib/BloqadeLattices/src/region.jl
@@ -192,7 +192,17 @@ Distance between two points.
 If just points `x` and `y` are provided, the Euclidean distance is calculated. 
 
 If a `region` is provided, then it is automatically assumed periodic boundary conditions are enabled
-and the closest possible distance between the two points is returned.
+and the smallest possible distance between the two points is returned.
+
+```jldoctest; setup=:(using BloqadeLattices)
+julia> distance((0.0, 0.0), (1.0, 1.0))
+1.4142135623730951
+
+julia> bounds = zeros(2,2); bounds[:,1] .= (3, 0); bounds[:,2] .= (0, 3);
+
+julia> distance(Parallelepiped(bounds), (0.5, 0.5), (2.5, 2.5))
+1.4142135623730951
+```
 """
 distance(x, y) = sqrt(mapreduce(x -> x^2, +, x .- y))
 

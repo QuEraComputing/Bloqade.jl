e67c3eca7d1a52e09f878c05ae2568670251ee08
John Long <johnzl@protonmail.ch>
2022-11-16 15:21:57 -0500

    Added docstrings to exported functions in capabilities.jl

--- lib/BloqadeSchema/src/capabilities.jl
+++ lib/BloqadeSchema/src/capabilities.jl
@@ -43,6 +43,23 @@ function convert_units_recursive(input_value,units)
     end
 end
 
+"""
+    get_device_capabilities(capabilities_file=nothing)
+
+Generates a `DeviceCapabilities` struct from either an explicitly provided path to a capabilities
+JSON file or using the default JSON provided in "lib/BloqadeSchema/config/capabilities-qpu1-mock.json".
+
+By default, the units for capabilities JSON file are specified by the 
+"lib/BloqadeSchema/config/capabilities-qpu1-mock-units.json" file this function
+gives a `DeviceCapabilities` struct with *non-SI base* (ex: μm, μs) units.
+
+See also [`get_device_capabilities_SI`](@ref)
+
+```jldoctest; 
+julia> get_device_capabilities()
+BloqadeSchema.DeviceCapabilities(BloqadeSchema.TaskCapabilities(1, 1000), BloqadeSchema.LatticeCapabilities(BloqadeSchema.LatticeAreaCapabilities(75.0, 76.0), BloqadeSchema.LatticeGeometryCapabilities(4.0, 4.0, 0.1, 256), 256), BloqadeSchema.RydbergCapabilities(5.42e6, BloqadeSchema.RydbergGlobalCapabilities(0.0, 15.8, 0.0004, 250.0, -125.0, 125.0, 2.0e-7, 2500.0, -99.0, 99.0, 5.0e-7, 0.0, 4.0, 0.001, 0.05), nothing))
+```
+"""
 function get_device_capabilities(capabilities_file=nothing)
     units_file = joinpath(pkgdir(@__MODULE__),"config","capabilities-qpu1-mock-units.json")
     capabilities_json_units = JSON.parse(JSON.open(units_file))["capabilities"]
@@ -61,7 +78,19 @@ end
 
 
 # leave as SI units, needed for rounding purposes
+"""
+    get_device_capabilities_SI(capabilities_file=nothing)
 
+Generates a `DeviceCapabilities` struct from either an explicitly provided path to a capabilities
+JSON file or using the default JSON provided in "lib/BloqadeSchema/config/capabilities-qpu1-mock.json".
+
+The values returned are in Base SI units: m, s, rad/s, etc.
+
+```jldoctest;
+julia> get_device_capabilities_SI()
+BloqadeSchema.DeviceCapabilities(BloqadeSchema.TaskCapabilities(1, 1000), BloqadeSchema.LatticeCapabilities(BloqadeSchema.LatticeAreaCapabilities(7.5e-5, 7.6e-5), BloqadeSchema.LatticeGeometryCapabilities(4.0e-6, 4.0e-6, 1.0e-7, 256), 256), BloqadeSchema.RydbergCapabilities(5.42e-24, BloqadeSchema.RydbergGlobalCapabilities(0.0, 1.58e7, 400.0, 2.5e14, -1.25e8, 1.25e8, 0.2, 2.5e15, -99.0, 99.0, 5.0e-7, 0.0, 4.0e-6, 1.0e-9, 5.0e-8), nothing))
+```
+"""
 function get_device_capabilities_SI(capabilities_file=nothing)
 
     capabilities_json = if isnothing(capabilities_file)
@@ -75,7 +104,14 @@ function get_device_capabilities_SI(capabilities_file=nothing)
 end
 
 
+"""
+    get_rydberg_capabilities(;device_capabilities::DeviceCapabilities=get_device_capabilities())
 
+Returns a named tuple containing values from the `RydbergCapabilities` field in `DeviceCapabilities`
+designed to be used by [`validate`](@ref). Remaps fields in the structs to easier to access names:
+(ex: `DeviceCapabilities.rydberg.global_value.time_delta_min` is now accessible as
+`Ω.min_time_step`).
+"""
 function get_rydberg_capabilities(;device_capabilities::DeviceCapabilities=get_device_capabilities())
     return if isnothing(device_capabilities.rydberg.local_value)
         (

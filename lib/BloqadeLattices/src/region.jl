bed120323de5ecbb5bd9f82cd7e50c7331c967df
John Long <johnzl@protonmail.ch>
2022-11-16 12:30:38 -0500

    Add implementation section for AbstractRegion

--- lib/BloqadeLattices/src/region.jl
+++ lib/BloqadeLattices/src/region.jl
@@ -2,6 +2,14 @@
     AbstractRegion{D}
 
 Supertype for all `D` dimensional regions used to define bounds on lattices.
+
+# Implementation
+
+The following should be overriden:
+
+- `Base.in`: Returns `Bool` on whether a point exists in the region
+- `Base.mod`: Maps points outside the region back into the region
+- `distance`: Calculates distance between points with periodic boundary conditions enabled
 """
 abstract type AbstractRegion{D} end
 

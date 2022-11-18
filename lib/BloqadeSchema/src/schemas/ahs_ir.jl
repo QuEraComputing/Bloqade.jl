8c61daeba388aa48f037330c724c4935fc50bc59
John Long <johnzl@protonmail.ch>
2022-11-18 10:05:26 -0500

    Added docstrings to ahs_ir.jl exported items

--- lib/BloqadeSchema/src/schemas/ahs_ir.jl
+++ lib/BloqadeSchema/src/schemas/ahs_ir.jl
@@ -53,6 +53,20 @@ end
     rydberg::RydbergHamiltonian
 end
 
+"""
+    struct TaskSpecification <: QuEraSchema
+
+The schema representation of a task for the machine.
+
+Is the output of [`to_schema`](@ref) and [`to_schema_no_validation`](@ref)
+as well as input to `[execute(task::TaskSpecification)]`(@ref).
+
+# Fields
+- `nshots::Int`: Number of shots (number of times hamiltonian is executed)
+- `lattice::Lattice`: The Bravais lattice vectors and sites
+- `effective_hamiltonian::EffectiveHamiltonian`: a `RydbergHamiltonian` instance 
+wrapped inside an `EffectiveHamiltonian`
+"""
 @option struct TaskSpecification <: QuEraSchema
     nshots::Int
     lattice::Lattice

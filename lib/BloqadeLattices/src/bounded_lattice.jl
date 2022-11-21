c823ab13487b68d09a73d0e4a805b9357ebed721
John Long <johnzl@protonmail.ch>
2022-11-21 10:35:54 -0500

    refer to region in code style vs stanard text

--- lib/BloqadeLattices/src/bounded_lattice.jl
+++ lib/BloqadeLattices/src/bounded_lattice.jl
@@ -7,7 +7,7 @@ Defines a lattice bounded by a region with the option for periodic boundary cond
 # Fields
 - `lattice <: AbstractLattice`: Lattice to be bounded.
 - `region <: AbstractRegion`: Region that bounds the underlying `lattice`.
-- `site_positions::AtomList`: Positions of the atoms inside region.
+- `site_positions::AtomList`: Positions of the atoms inside `region`.
 - `pbc::Bool`: Enable/Disable behavior for Periodic Boundary Conditions.
 
 """

53db09eb4b1187da93451b38d4dd4fcaa243c889
Anna Knoerr <an.kn@protonmail.ch>
2022-11-28 23:46:16 +0100

    replaced t-tests with chi-squared tests

--- lib/BloqadeQMC/test/rydberg.jl
+++ lib/BloqadeQMC/test/rydberg.jl
@@ -38,7 +38,8 @@ end
 
 ### Now, start running QMC tests.
 
-THRESHOLD = 2.576
+# THRESHOLD_t = 2.576             threshold for t-test with ∞ DOF and 99.5% confidence   
+THRESHOLD_χ = 43.77             # threshold for χ² test with 30 DOF and p=0.05
 
 R_b = 7.74                      # This does correspond to C6 = 862690 * 2pi Mhz μm^6 at a Rabi frequency of Ω = 2π * 4.
 Ω = 2π * 4
@@ -57,6 +58,8 @@ lat = Chain(N, a, false; trunc=Inf)
 
     energy_QMC_β1 = []
 
+    χ_squared = 0
+
     for ii in 1:Δ_step
         H = Rydberg(lat, R_b, Ω, Δ[ii])
         ts = BinaryThermalState(H, M)
@@ -71,9 +74,11 @@ lat = Chain(N, a, false; trunc=Inf)
         end
 
         append!(energy_QMC_β1, mean_and_stderr(x -> -x/β, ns) + H.energy_shift)
+        χ_squared += abs2(value(energy_QMC_β1[ii]) - energy_ED[1, ii]) / abs2(uncertainty(energy_QMC_β1[ii]))
 
-        @test abs(stdscore(energy_QMC_β1[ii], energy_ED[1,ii])) < THRESHOLD
+        # @test abs(stdscore(energy_QMC_β1[ii], energy_ED[1,ii])) < THRESHOLD_t       t-test for testing each QMC run individually
     end
+    @test χ_squared < THRESHOLD_χ
 end
 
 
@@ -83,9 +88,11 @@ end
     MCS = 100_000
     M = 500
 
-    rng = MersenneTwister(4123)
+    rng = MersenneTwister(13542)
 
     energy_QMC_β2 = []
+
+    χ_squared = 0
 
     for ii in 1:Δ_step
         H = Rydberg(lat, R_b, Ω, Δ[ii])
@@ -101,9 +108,10 @@ end
         end
 
         append!(energy_QMC_β2, mean_and_stderr(x -> -x/β, ns) + H.energy_shift)
-
-        @test abs(stdscore(energy_QMC_β2[ii], energy_ED[2,ii])) < THRESHOLD
+        χ_squared += abs2(value(energy_QMC_β2[ii]) - energy_ED[2, ii]) / abs2(uncertainty(energy_QMC_β2[ii]))
+        # @test abs(stdscore(energy_QMC_β2[ii], energy_ED[2,ii])) < THRESHOLD_t
     end
+    @test χ_squared < THRESHOLD_χ
 end
 
 @testset "1D Chain (9 atoms), β=0.5" begin
@@ -115,6 +123,8 @@ end
     rng = MersenneTwister(12345)
 
     energy_QMC_β3 = []
+
+    χ_squared = 0
 
     for ii in 1:Δ_step
         H = Rydberg(lat, R_b, Ω, Δ[ii])
@@ -131,6 +141,9 @@ end
 
         append!(energy_QMC_β3, mean_and_stderr(x -> -x/β, ns) + H.energy_shift)
 
-        @test abs(stdscore(energy_QMC_β3[ii], energy_ED[3,ii])) < THRESHOLD
+        χ_squared += abs2(value(energy_QMC_β3[ii]) - energy_ED[3, ii]) / abs2(uncertainty(energy_QMC_β3[ii]))
+
+        # @test abs(stdscore(energy_QMC_β3[ii], energy_ED[3,ii])) < THRESHOLD_t
     end
+    @test χ_squared < THRESHOLD_χ
 end

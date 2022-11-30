43b0bb6e6850bb1c37ec74436814f691d2b7bf38
Anna Knörr <61286790+Atomyka@users.noreply.github.com>
2022-11-30 22:11:38 +0100

    accept AbstractMatrix as type
    Co-authored-by: Phillip Weinberg <weinbe58@gmail.com>

--- lib/BloqadeQMC/src/ising/Rydberg.jl
+++ lib/BloqadeQMC/src/ising/Rydberg.jl
@@ -30,7 +30,7 @@ nspins(H::Rydberg) = length(H.atoms)
 @inline diagonaloperator(H::AbstractRydberg) = diagonaloperator(typeof(H))
 
 
-function make_prob_vector(H::Type{<:AbstractRydberg}, V::Matrix{T}, Ω::AbstractVector{T}, δ::AbstractVector{T}; epsilon=0.0) where T
+function make_prob_vector(H::Type{<:AbstractRydberg}, V::AbstractMatrix{T}, Ω::AbstractVector{T}, δ::AbstractVector{T}; epsilon=0.0) where T
     @assert length(Ω) == length(δ) == size(V, 1) == size(V, 2)
     @assert (0.0 <= epsilon <= 1.0) "epsilon must be in the range [0, 1]!"
 

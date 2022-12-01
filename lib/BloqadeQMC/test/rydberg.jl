331d218422f44d0fa8c1305d3cbe660b23b21110
Anna Knoerr <an.kn@protonmail.ch>
2022-12-01 19:56:59 +0100

    added site-dependent test

--- lib/BloqadeQMC/test/rydberg.jl
+++ lib/BloqadeQMC/test/rydberg.jl
@@ -181,6 +181,73 @@ end
     # scatter!(Δ/2π, energy_ED[3,:], marker=:x)
 end
 
+#########################
+
+# Generate ED values for test with site-dependent parameters
+
+β = 0.005
+nsites = 9
+atoms = generate_sites(ChainLattice(), nsites, scale = 5.48)
+
+Ω_step = 9
+Δ_step = 30
+Ω = collect(LinRange(-2π * 0, 2π * 4, Ω_step))
+Δ = LinRange(-2π * 9, 2π * 9, Δ_step)
+
+energy_ED_sites = zeros(Δ_step)
+
+for ii in 1:Δ_step
+    h_ii = rydberg_h(atoms; Δ = Δ[ii], Ω)
+    h_m = Matrix(mat(h_ii)) 
+    energies, vecs = LinearAlgebra.eigen(h_m) 
+    N_e = length(energies)
+
+    w = exp.(-β .* (energies .- energies[1]))
+    energy_ED_sites[ii] = sum(w .* energies) / sum(w)
+end
+
+
 @testset "1D Chain with site-dependent parameters" begin
+    EQ_MCS = 30
+    MCS = 100_000
+    M = 50
+
+    rng = MersenneTwister(3214)
+
+    energy_QMC_sites = []
+
+    χ_squared = 0
+
+    for ii in 1:Δ_step
+        @show ii
+        h_ii = rydberg_h(atoms; Δ = Δ[ii], Ω)
+        H = rydberg_QMC(h_ii)
+        ts = BinaryThermalState(H, M)
+        d = Diagnostics()
     
+        [mc_step_beta!(rng, ts, H, β, d, eq=true) for i in 1:EQ_MCS] #equilibration phase
+    
+        ns = zeros(MCS)
+    
+        for i in 1:MCS # Monte Carlo Steps
+            ns[i] = mc_step_beta!(rng, ts, H, β, d, eq=false)
+        end       
+
+        # Binning analysis 
+        energy(x) = -x / β + H.energy_shift
+        BE = LogBinner(energy.(ns))
+        τ_energy = tau(BE)
+        ratio = 2 * τ_energy + 1
+        energy_binned = measurement(mean(BE), std_error(BE)*sqrt(ratio)) 
+        append!(energy_QMC_sites, energy_binned)
+
+        χ_squared += abs2(value(energy_QMC_sites[ii]) - energy_ED_sites[ii]) / abs2(uncertainty(energy_QMC_sites[ii]))
+        @show χ_squared
+        # @test abs(stdscore(energy_QMC_β1[ii], energy_ED[1,ii])) < THRESHOLD_t       t-test for testing each QMC run individually
+    end
+
+    @test χ_squared < THRESHOLD_χ
+
+    # scatter(Δ/2π, value.(energy_QMC_sites); yerror=uncertainty.(energy_QMC_sites), marker=:x)
+    # scatter!(Δ/2π, energy_ED_sites, marker=:x)
 end

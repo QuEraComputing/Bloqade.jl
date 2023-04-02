f8e4230327b636ce023491eeebb9731a5e24a01a
Phillip Weinberg <weinbe58@gmail.com>
2023-04-02 09:20:58 -0400

    using OrderedDict to be consistent with Configurations.jl

--- lib/BloqadeSchema/src/serialize.jl
+++ lib/BloqadeSchema/src/serialize.jl
@@ -1,3 +1,5 @@
+using OrderedCollections: OrderedDict
+
 function Configurations.to_dict(
     ::Type{T},
     x::Union{RabiFrequencyAmplitude,RabiFrequencyPhase},
@@ -8,13 +10,13 @@ end
 
 function Configurations.to_dict(::Type{T}, x::Union{Detuning,RydbergCapabilities}, option::Configurations.ToDictOption) where {T}
     if isnothing(x.local_value)
-        return Dict("global" => Configurations.to_dict(x.global_value))
+        return OrderedDict("global" => Configurations.to_dict(x.global_value))
     end
 
-    return Dict("global" => Configurations.to_dict(x.global_value), "local" => Configurations.to_dict(x.local_value))
+    return OrderedDict("global" => Configurations.to_dict(x.global_value), "local" => Configurations.to_dict(x.local_value))
 end
 
 
 function Configurations.to_dict(::Type{T}, x::Lattice, option::Configurations.ToDictOption) where {T}
-    return Dict("sites" => map(collect, x.sites), "filling" => x.filling)
+    return OrderedDict("sites" => map(collect, x.sites), "filling" => x.filling)
 end

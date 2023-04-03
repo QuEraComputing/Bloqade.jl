790293f28a94d11ff0bb0cff201f88da693095a7
John Long <johnzl@protonmail.ch>
2023-04-03 13:15:00 -0400

    fix indexing

--- lib/BloqadeExpr/src/linalg.jl
+++ lib/BloqadeExpr/src/linalg.jl
@@ -27,7 +27,7 @@ end
 # Parallelized Diagonal multiplication
 function bmul!(C, A::Diagonal, B, α, β)
     @batch for i in eachindex(B)
-        @inbounds C[I] = A.diag[i] * B[i] * α + β * C[I] 
+        @inbounds C[i] = A.diag[i] * B[i] * α + β * C[i] 
     end
     return C
 end

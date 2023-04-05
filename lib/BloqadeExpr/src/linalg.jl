58a1b5123a26705494ff2f5e9afb838607c3b07a
John Long <johnzl@protonmail.ch>
2023-04-04 23:03:51 -0400

    Moved bmul! for SparseMatrixCSR into BloqadeExpr

--- lib/BloqadeExpr/src/linalg.jl
+++ lib/BloqadeExpr/src/linalg.jl
@@ -32,7 +32,39 @@ function bmul!(C, A::Diagonal, B, α, β)
     return C
 end
 
+# Taken directly from ThreadedSparseCSR. ThreadedSparseCSR currently only works with Polyester 0.5, which
+# has known issues with thread count > 64. Moving it here allows us to support more recent versions of
+# Polyester.
+"""
+    bmul!(y::AbstractVector, A::SparseMatrixCSR, x::AbstractVector, alpha::Number, beta::Number)
+    bmul!(y::AbstractVector, A::SparseMatrixCSR, x::AbstractVector)
+Evaluates `y = alpha*A*x + beta*y` (`y = A*x`)
+In-place multithreaded version of sparse csr matrix - vector multiplication, using the threading provided by Polyester.jl
+"""
+function bmul!(y::AbstractVector, A::SparseMatrixCSR, x::AbstractVector, alpha::Number, beta::Number)
+    
+    A.n == size(x, 1) || throw(DimensionMismatch())
+    A.m == size(y, 1) || throw(DimensionMismatch())
+
+    o = getoffset(A)
+    
+    @batch minbatch = size(y, 1) ÷ matmul_num_threads[] for row in 1:size(y, 1)
+        @inbounds begin
+            accu = zero(eltype(y))
+            for nz in nzrange(A, row)
+                col = A.colval[nz] + o
+                accu += A.nzval[nz]*x[col]
+            end
+            y[row] = alpha*accu + beta*y[row]
+    
+        end
+    end
+
+    return y
+
+end
+
 # dispatch to ThreadedSparseCSR
-bmul!(C, A::SparseMatrixCSR, B, α, β) = ThreadedSparseCSR.bmul!(C, A, B, α, β) 
+# bmul!(C, A::SparseMatrixCSR, B, α, β) = ThreadedSparseCSR.bmul!(C, A, B, α, β) 
 # dispatch to ParallelMergeCSR
 bmul!(C, A::Transpose{<:Any, <:SparseMatrixCSC}, B, α, β) = ParallelMergeCSR.mul!(C, A, B, α, β) 

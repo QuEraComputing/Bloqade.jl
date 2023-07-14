e5c9c1b5e40ba20705262885b12edfa68967ce05
Kai-Hsin Wu <khwu@KHWus-MacBook-Pro.local>
2023-07-14 11:49:30 -0400

    fix missing eltype for ThreadedMatrix

--- lib/BloqadeExpr/src/Lowlevel/types.jl
+++ lib/BloqadeExpr/src/Lowlevel/types.jl
@@ -25,7 +25,7 @@ end
 Base.size(m::ThreadedMatrix) = size(m.matrix)
 Base.size(m::ThreadedMatrix, i) = size(m.matrix)[i]
 Base.pointer(m::T) where {T <: Diagonal} = pointer(m.diag)
-
+Base.eltype(m::T) where {T <: ThreadedMatrix} = eltype(m.matrix) 
 
 precision_type(m::T) where {T <: Number} = real(typeof(m))
 precision_type(m::T) where {T <: Diagonal} = real(eltype(m))

40ddbba667b5efb9d2f1c2ee40fc1a50e6d34815
John Long <johnzl@protonmail.ch>
2024-01-05 09:30:15 -0500

    simple DP5 test

--- lib/BloqadeODE/test/dormandp.jl
+++ lib/BloqadeODE/test/dormandp.jl
@@ -1,8 +1,29 @@
-using BloqadeODE: BloqadeSolver, DP8Solver, SolverIterator, integrate!
+using Test
+using BloqadeODE: BloqadeSolver, DP8Solver, DP5Solver, SolverIterator, integrate!
 using BloqadeExpr: rydberg_h, nqudits
 using Bloqade
 using YaoArrayRegister
 
+
+@testset "Single End Times" begin
+    @testset "DP5" begin
+        atoms = [(i,) for i in 1:5]
+        h = rydberg_h(atoms; C = 2π * 109.16, Ω = sin, ϕ = cos)
+
+        # DormandPrince.jl 
+        dp_reg = zero_state(5)
+        bs = BloqadeSolver(dp_reg, 0.0, h; solver_type=DP5Solver)
+        integrate!(bs, 1.0)
+
+        # OrdinaryDiffEq 
+        tspan = (0, 1.0)
+        ode_reg = zero_state(5)
+        problem = SchrodingerProblem(ode_reg, tspan, h; algo=DP5())
+        emulate!(problem)
+
+        @test dp_reg ≈ ode_reg 
+    end
+end
 
 h = rydberg_h([(0,0), (0,6)], Ω=t -> sin(t))
 reg = zero_state(nqudits(h))

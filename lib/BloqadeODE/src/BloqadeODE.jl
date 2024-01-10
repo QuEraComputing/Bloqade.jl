52e115b7b7caffac8110486b707c0e4b50622620
Phillip Weinberg <weinbe58@gmail.com>
2024-01-10 14:01:18 -0500

    upgrading compatibility for SciMLBase. (#643)
    * upgrading compatibility for SciMLBase.
    
    * bump patch
    
    * removing imports of non-public APIs for DiffEqBase
    
    * updating deps for documentation build.

--- lib/BloqadeODE/src/BloqadeODE.jl
+++ lib/BloqadeODE/src/BloqadeODE.jl
@@ -9,44 +9,7 @@ using YaoSubspaceArrayReg
 @reexport using BloqadeExpr
 @reexport using OrdinaryDiffEq
 using BloqadeExpr: Hamiltonian
-using OrdinaryDiffEq: AutoSwitchCache, CompositeInterpolationData
 using LinearAlgebra
-
-using OrdinaryDiffEq:
-    @logmsg,
-    isadaptive,
-    gamma_default,
-    qmin_default,
-    qmax_default,
-    qsteady_min_default,
-    qsteady_max_default,
-    ODE_DEFAULT_NORM,
-    ODE_DEFAULT_ISOUTOFDOMAIN,
-    ODE_DEFAULT_UNSTABLE_CHECK,
-    ODE_DEFAULT_PROG_MESSAGE,
-    alg_extrapolates,
-    DefaultInit,
-    is_mass_matrix_alg,
-    LogLevel,
-    OrdinaryDiffEqAdaptiveAlgorithm,
-    DAEAlgorithm,
-    recursive_bottom_eltype,
-    recursive_unitless_bottom_eltype,
-    recursive_unitless_eltype,
-    recursivecopy,
-    initialize_tstops,
-    initialize_saveat,
-    initialize_d_discontinuities,
-    uses_uprev,
-    default_controller,
-    DEOptions,
-    OrdinaryDiffEqCompositeAlgorithm,
-    InterpolationData,
-    isdtchangeable,
-    fsal_typeof,
-    ODEIntegrator,
-    initialize_callbacks!,
-    handle_dt!
 
 export SchrodingerProblem, SchrodingerEquation
 

a30364b3e08c7572e872947a906d648d9fd673cb
Phillip Weinberg <weinbe58@gmail.com>
2024-01-22 20:01:46 -0500

    moving copy into script.

--- .ci/src/example.jl
+++ .ci/src/example.jl
@@ -147,6 +147,17 @@ in parallel.
         project_dir = joinpath(\"$example_dir\", name)
         isdir(project_dir) || continue
 
+        build_dir = joinpath(\"$build_dir\", name)
+        run(`mkdir -p  \$build_dir`)
+
+        for sub_dir in readdir(project_dir)
+            abs_path = joinpath(project_dir, sub_dir)
+            if isdir(abs_path) && sub_dir == "data"
+                @info "copying \$abs_path to build \$build_dir" 
+                run(`cp -r \$abs_path \$build_dir`)
+            end
+        end
+
         Pkg.activate(project_dir)
         Pkg.instantiate()
         CondaPkg.resolve()
@@ -154,7 +165,7 @@ in parallel.
         @info "building" project_dir
         Literate.$target(
             joinpath(project_dir, "main.jl"),
-            joinpath(\"$build_dir\", name),
+            build_dir,
             ;execute=$eval
         )
     end
@@ -164,12 +175,6 @@ in parallel.
     # then we run the build in one process
     # so that we can share compile results
     foreach_example() do path
-        example_dir = splitpath(path)[end]
-        for subdir in readdir(path)
-            fullpath = joinpath(path, subdir)
-            tutorial_path = 
-            isdir(fullpath) && subdir == "data" && run(`cp -r $fullpath $(joinpath(build_dir, example_dir))`)
-        end
         return dev(path)
     end
     return run(`$(Base.julia_exename()) --project=$ci_dir -e $script`)

11d90434075ba352509aebe7a52e86b014e7578d
fanglifl <lflnankai@gmail.com>
2024-01-23 10:02:24 -0500

    cut down the contents, facilating build docs

--- examples/8.simulating-noise/main.jl
+++ examples/8.simulating-noise/main.jl
@@ -136,7 +136,6 @@ ylims!(0,1)
 
 
 
-
 # ![BloqadeNoisy](../../../assets/BloqadeNoisy_tutorial/noisy_depolarizingrabi.png)
 
 # ## Coherent noise in neutral atom simulators
@@ -267,7 +266,7 @@ new_error_model = load_error_model(default_error_model);
 Ω = 2π
 C = 862690 * 2π
 Rb = (C/Ω)^(1/6)
-N = 14
+N = 8
 
 atoms = generate_sites(ChainLattice(), N, scale = .65*Rb)
 Δ_NNN = 1/(2N) * sum(
@@ -299,7 +298,7 @@ H = rydberg_h(atoms; Ω = Ω, Δ = Δ)
 
 expt_times = LinRange(t_prep, t_prep+t_quench, 70)
 prob = NoisySchrodingerProblem(zero_state(N), expt_times, H, Aquila())
-@time sim = emulate_noisy(prob, 500, [mat(chain(igate(N)-put(N, i=>Op.n) for i in 1:N))];
+@time sim = emulate_noisy(prob, 200, [mat(chain(igate(N)-put(N, i=>Op.n) for i in 1:N))];
     readout_error = true, report_error = true, shots = 1000,
     ensemble_algo = EnsembleThreads()
 )
@@ -317,138 +316,6 @@ scatter!(expt_times, sim.expectations,
 
 # ![BloqadeNoisy](../../../assets/BloqadeNoisy_tutorial/noisy_manybodyscar.png)
 
-# ## Manybody Fidelity
-# A Haar-random pure state ``|\psi\rangle`` from a Hilbert space with dimension ``D = 2^N`` produces probability amplitudes ``p(z) = |\langle z |\psi\rangle|^2`` that are distributed exponentially according to the Porter-Thomas distribution ``P_1(p) = De^{-Dp}`` in the limit of large ``D``. Haar-random sates are produced by chaotic quantum evolution, which we can achieve by randomly spacing ``N`` atoms and choosing ``\Omega(t)`` to be strong relative to the coupling and vary sufficiently over the interval. Under the influence of noise, the evolution can be desribed by
-# ```math
-# \rho(t) = F|\psi(t)\rangle \langle \psi(t)| + (1-F(t))\chi
-# ```
-# where ``\chi`` is a density matrix with ``r(z) = \langle z|\chi|z\rangle`` strongly peaked around ``1/D``, approximately ``P_2(r) = \delta(z-\frac{1}{D})``. The probability ``q(z) = \langle z|\rho|z\rangle`` is thus distributed according to the convolution
-# ```math
-# P_3(q) = \int_0^{q}\frac{1}{1-F}\delta\left(\frac{z}{1-F}-\frac{1}{D}\right)\frac{1}{F}De^{-D(q-z)/F}dz = \begin{cases}
-# 0 & q < (1-F)/D\\
-# \frac{D}{F}e^{(1-F)/F}e^{-Dq/F} & \text{otherwise}
-# \end{cases}
-# ```
-# This distribution is the one described in [5].
-#
-# We consider single-atom depolarizing noise at a rate ``\gamma``, and we take ``F(t)`` to be the probability that no error has occurred after time ``t``. Since there are ``3N`` possible errors ocurring at a rate ``\gamma``, we find ``F(t) = 1-e^{-3N\gamma t}``.
-#
-# ### Implementation in Bloqade
-# First, we set up a Hamiltonian and simulate noiseless evolution:
-
-tend = 10
-Ω = Waveform(t-> 2.5*2π*(sin(sqrt(10)*2π*t)+cos(sqrt(2)*2π*t)), tend)
-R = (862690/2.5)^(1/6)
-
-N = 12
-D = 2^N
-atoms = [(R*i+.05*randn(),) for i in 1:N]
-
-h = rydberg_h(atoms; Ω = Ω)
-
-ψ = solve(SchrodingerProblem(zero_state(N), tend, h), DP8()).u[end]
-
-# First, we construct a single-atom depolarizing model on ``N`` qubits. Then, we repeat the simulation with different depolarizing rates. The `emulate` method can take a function of the solution to save. We save two quantities: the probability amplitudes ``|\langle z | \psi \rangle |^2`` and the fidelity ``F = \langle \psi| \rho |\psi \rangle``. 
-
-depol(γ, N) = [sqrt(γ)*mat(put(N, i=>op)) for i in 1:N for op in [X,Y,Z]]
-function sim_depol(γ)
-    prob = NoisySchrodingerProblem(
-        zero_state(N), 
-        [0.0,tend], 
-        h, 
-        depol(γ, N)
-    )
-    sim = emulate_noisy(prob, 500, 
-        sol -> [
-            abs.(sol[end]).^2, 
-            abs(sol[end]' * ψ)^2
-        ];
-        ensemble_algo = EnsembleThreads()
-    )
-    (simulation_series_mean(sim),
-        simulation_series_err(sim))
-end
-depol_strengths = [.002, .004, .006]
-p = [sim_depol(γ) for γ in depol_strengths];
-
-# Lastly, we create a histogram of the probability amplitudes from the simulation for each level of depolarizing noise, and compare against the prediction:
-
-colors = [:blue, :green, :purple]
-
-plot(xlabel = L"p", ylabel = L"P(p)", title = "Probability distribution vs noise strength")
-stephist!(abs.(ψ).^2, yaxis =:log, normalize =true, color = :black, label = "γ = 0")
-plot!(x->D*exp(-D*x), 0, .002, color = :black, linestyle = :dash, label = "")
-
-for (p, c, r) in zip(p, colors, depol_strengths)
-    F = exp(-r * 3N * tend)
-    stephist!(p[1][1], yaxis =:log, normalize =true, color = c, label = "γ = $(@sprintf("%.3f", r))")
-    plot!(x->D/F*exp((1-F)/F)*exp(-D* x/F), (1-F)/D, .002, color = c, linestyle = :dash, label = "")
-    plot!([(1-F)/D, (1-F)/D], [0, D/F], color = c, linestyle = :dash, label = "")
-end
-
-ylims!(10, 2f4)
-xlims!(0, .0015)
-
-# ![BloqadeNoisy](../../../assets/BloqadeNoisy_tutorial/noisy_depol_dists.png)
-
-# We can examine three different measures of fidelity: First, we have the actual state fidelity ``F = \langle \psi | \rho |\psi \rangle`` which is saved in the simulation. Next, we have the prediction from the depolarizing model ``F = e^{-3N\gamma t}``. Lastly, there is another quantity called the linear cross-entropy which acts as a proxy for the fidelity with chaotic Hamiltonians. The cross entropy is defined
-# ```math
-# \text{XEB} = \langle D^2 p(z)q(z) - 1\rangle
-# ```
-# where ``p(z) = |\langle z | \psi \rangle|^2`` and ``q(z) = \langle z | \rho |z \rangle``. Using the form from above ``\rho = F|\psi\rangle \langle \psi| + (1-F)\chi``, we see that
-# ```math
-# \langle p(z) q(z) \rangle = F\langle p^2(z)\rangle + (1-F)\langle p(z) r(z) \rangle
-# ```
-# Using the fact that ``P_1(p) = De^{-Dp}`` and ``P_2(r) = \delta(r-1/N)``, we compute
-# ```math
-# \langle p^2(z)\rangle  = D\int_0^\infty p^2e^{-Dp}dp = \frac{2}{D^2}
-# ```
-# and
-# ```math
-# \langle p(z) r(z) \rangle = D\int_0^\infty\int_0^\infty p\delta(r-\frac{1}{D})e^{-Dp/r}dpdr = \frac{1}{D^2}
-# ```
-# Substituting these in gives ``\text{XEB} = D^2(2/D^2)F + D^2(1-F)/D^2 - 1 = F``. Thus all three quantities estimate ``F``.
-
-plot(depol_strengths, [p[i][1][2] for i in 1:3], title = "Depolarizing fidelity and cross-entropy", xlabel = L"\gamma", ylabel = "fidelity", label = L"F", yerr = [p[i][2][2] for i in 1:3])
-plot!(depol_strengths, exp.(-3N * tend .* depol_strengths), label = L"e^{-3\gamma N T}")
-plot!(depol_strengths, [(2^N*sum(p[i][1][1] .* abs.(ψ).^2)-1) for i in 1:3], label = "XEB")
-
-# ![BloqadeNoisy](../../../assets/BloqadeNoisy_tutorial/noisy_fidelitymeasures.png)
-
-# ## Memory constraints
-# When simulating large systems, memory can be an issue. The `NoisySchrodingerProblem` can also be used directly with the `DifferentialEquations` interface to simulate each trajectory manually if more control is required. The `randomize` function reinitializes the trajectory with a new sample from the specified distirbution of Hamiltonian parameters and chooses a random initial condition.
-
-R = (862690/2.5)^(1/6)
-atoms = generate_sites(SquareLattice(), 4, 4, scale = .85*R)
-N = length(atoms)
-tend = 3.0
-Ω = 2.5*2π
-
-h = rydberg_h(atoms; Ω = Ω, Δ = 0)
-
-ψ = solve(SchrodingerProblem(zero_state(N), tend, h), DP8()).u;
-
-p = zeros(2^N)
-ntraj = 250
-prob = NoisySchrodingerProblem(zero_state(N), [tend], h, Aquila())
-for i in ProgressBar(1:ntraj)
-    sample = randomize(prob)
-    int = init(sample, DP8())
-    solve!(int)
-    p .+= abs.(int.u).^2/ntraj
-end
-
-scatter(
-    [real(ψ[end]' * mat(put(N, i=>Op.n)) * ψ[end]) for i in 1:N],
-    marker = :circle, markersize = 6, title = "Noisy vs noiseless excitation number",
-    xlabel = "site #",
-    ylabel = L"\langle \hat n_i \rangle",
-    label = "ideal"
-    )
-scatter!([expectation_value_noisy(Aquila(), p, mat(put(N, i=>Op.n))) for i in 1:N], marker = :square, markersize = 6, label = "noisy")
-ylims!(0,1)
-
-# ![BloqadeNoisy](../../../assets/BloqadeNoisy_tutorial/noisy_numberdensity.png)
 
 # ## References & Further Reading
 # [1] https://qutip.org/docs/latest/guide/dynamics/dynamics-monte.html
@@ -458,5 +325,3 @@ ylims!(0,1)
 # [3] Wurtz, J. et al. (2023). Aquila: QuEra's 256-qubit neutral-atom quantum computer. arXiv preprint arXiv:2306.11727.
 #
 # [4] Daniel, Aiden, et al. "Bridging quantum criticality via many-body scarring." Physical Review B 107.23 (2023): 235108.
-#
-# [5] Boixo, Sergio, et al. "Characterizing quantum supremacy in near-term devices." Nature Physics 14.6 (2018): 595-600.

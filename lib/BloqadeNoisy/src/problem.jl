f6c875a6496e93189f4c017c557c446d2ecf0ec6
GiggleLiu <cacate0129@gmail.com>
2024-08-02 01:23:40 +0800

    fix test

--- lib/BloqadeNoisy/src/problem.jl
+++ lib/BloqadeNoisy/src/problem.jl
@@ -453,11 +453,11 @@ Convenience method to estimate the sampling error in the ensemble solution
 """
 function simulation_series_err(sim; index=false, factor=2)
     ntraj = length(sim)
-    times = length(sim[:, 1])
+    times = length(sim.u[1])
     if index == false
-        [factor * std([sim[i][t] for i in 1:ntraj]) / sqrt(ntraj) for t in 1:times]
+        [factor * std([sim.u[i][t] for i in 1:ntraj]) / sqrt(ntraj) for t in 1:times]
     else
-        [factor * std([sim[i][t][index] for i in 1:ntraj]) / sqrt(ntraj) for t in 1:times]
+        [factor * std([sim.u[i][t][index] for i in 1:ntraj]) / sqrt(ntraj) for t in 1:times]
     end
 end
 
